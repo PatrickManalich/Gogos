00200a6b4545697fb44c4937032ce35c972b552c
PatrickManalich <patrickmanalich@gmail.com>
2021-04-25 21:33:38 -0400

    Create attribute scripts

---
+++ GogosUnity/Assets/Scripts/Attributes/BlastableAttribute.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+	public class BlastableAttribute : MonoBehaviour
+	{
+        private const float FalloffRate = 1.5f;
+
+        [SerializeField]
+        private TriggerListener m_TriggerListener;
+
+        [SerializeField]
+        private Rigidbody m_Rigidbody;
+
+        [SerializeField]
+        private GameObject m_ForcePoint;
+
+        private void Awake()
+		{
+            m_TriggerListener.Entered += TriggerListener_OnEntered;
+		}
+
+        private void OnDestroy()
+		{
+			m_TriggerListener.Entered -= TriggerListener_OnEntered;
+		}
+
+		private void TriggerListener_OnEntered(object sender, TriggerEventArgs e)
+		{
+            var blastTrigger = e.OtherCollider.GetComponent<BlastTrigger>();
+            if (blastTrigger)
+            {
+                var blastCenterPosition = e.OtherCollider.transform.position;
+                var oppositeDirection = (transform.position - blastCenterPosition).normalized;
+                var upwardsDirection = Vector3.up * blastTrigger.BlastForceTierTracker.BlastUpwardsModifier;
+                var forceDirection = oppositeDirection + upwardsDirection;
+
+                var distance = Vector3.Distance(transform.position, blastCenterPosition);
+                var distanceMultiplier = Mathf.Max(0, blastTrigger.BlastForceTierTracker.BlastForce - FalloffRate * distance);
+
+                m_Rigidbody.AddForceAtPosition(forceDirection * distanceMultiplier, m_ForcePoint.transform.position, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/Attributes/SupportableAttribute.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class SupportableAttribute : MonoBehaviour
+    {
+        [SerializeField]
+        private TriggerListener m_TriggerListener;
+
+        [SerializeField]
+        private AbstractTierTracker[] m_TierTrackers;
+
+        private void Awake()
+        {
+            m_TriggerListener.Entered += TriggerListener_OnEntered;
+            m_TriggerListener.Exited += TriggerListener_OnExited;
+        }
+
+        private void OnDestroy()
+        {
+            m_TriggerListener.Exited -= TriggerListener_OnExited;
+            m_TriggerListener.Entered -= TriggerListener_OnEntered;
+        }
+
+        private void TriggerListener_OnEntered(object sender, TriggerEventArgs e)
+        {
+            var supportTrigger = e.OtherCollider.GetComponent<SupportTrigger>();
+            if (supportTrigger)
+            {
+                foreach (var tierTracker in m_TierTrackers)
+                {
+                    supportTrigger.SupportAbility.ProvideSupport(tierTracker);
+                }
+            }
+        }
+
+        private void TriggerListener_OnExited(object sender, TriggerEventArgs e)
+        {
+            var supportTrigger = e.OtherCollider.GetComponent<SupportTrigger>();
+            if (supportTrigger)
+            {
+                foreach (var tierTracker in m_TierTrackers)
+                {
+                    supportTrigger.SupportAbility.RemoveSupport(tierTracker);
+                }
+            }
+        }
+    }
+}
