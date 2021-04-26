9b5f4b2318af282c72b5e9341e59bd840c79286f
PatrickManalich <patrickmanalich@gmail.com>
2021-04-25 21:31:36 -0400

    Create SupportAbility script

---
+++ GogosUnity/Assets/Scripts/SupportAbility.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+	public class SupportAbility : MonoBehaviour
+	{
+        [SerializeField]
+        private TierVariant m_TierVariant;
+
+        [Range(-3, 3)]
+        [SerializeField]
+        private int m_TierModifier;
+
+        public void ProvideSupport(AbstractTierTracker tierTracker)
+        {
+            if (tierTracker.TierVariant == m_TierVariant)
+            {
+                tierTracker.Modify(m_TierModifier);
+            }
+        }
+
+        public void RemoveSupport(AbstractTierTracker tierTracker)
+        {
+            if (tierTracker.TierVariant == m_TierVariant)
+            {
+                tierTracker.Modify(-m_TierModifier);
+            }
+        }
+    }
+}
