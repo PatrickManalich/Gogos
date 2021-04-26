4aba4828804141b19ff5535ce87e258805c86c14
PatrickManalich <patrickmanalich@gmail.com>
2021-04-25 21:28:44 -0400

    Create extension scripts

---
+++ GogosUnity/Assets/Scripts/Extensions/MathExtensions.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gogos.Extensions
+{
+    public static class MathExtensions
+    {
+        /// <summary>
+        /// Returns this float rounded up or down based on the specified midpoint.
+        /// </summary>
+        public static float RoundAt(this float f, float midpoint)
+        {
+            midpoint += Mathf.Floor(f);
+            return f < midpoint ? Mathf.Floor(f) : Mathf.Ceil(f);
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the time in seconds it took to complete the last frame.
+        /// </summary>
+        public static float WithDeltaTime(this float f)
+        {
+            return f * Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the interval in seconds at which physics are performed.
+        /// </summary>
+        public static float WithFixedDelaTime(this float f)
+        {
+            return f * Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the timeScale-independent time in seconds it took to complete the last frame.
+        /// </summary>
+        public static float WithUnscaledDeltaTime(this float f)
+        {
+            return f * Time.unscaledDeltaTime;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance taken to the specified exponential power.
+        /// </summary>
+        public static float ToPower(this float f, int exponent)
+        {
+            float total = 1;
+            if (exponent < 0)
+            {
+                LoopAction(exponent * -1, (int i) => total *= i % 2 == 0 ? f * -1 : f);
+                total = 1 / total;
+            }
+            else
+            {
+                LoopAction(exponent, (int i) => total *= f);
+            }
+            return total;
+        }
+
+        /// <summary>
+        /// Loop a single action a specified number of times.
+        /// </summary>
+        public static void LoopAction(int iterations, Action<int> step)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                step(i);
+            }
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the value of this instance.
+        /// </summary>
+        public static float Squared(this float f)
+        {
+            return f * f;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance taken to the 3rd power.
+        /// </summary>
+        public static float Cubed(this float f)
+        {
+            return f * f * f;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the value of this instance.
+        /// </summary>
+        public static int Squared(this int i)
+        {
+            return i * i;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the value of this instance.
+        /// </summary>
+        public static double Squared(this double d)
+        {
+            return d * d;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance multiplied by the value of this instance.
+        /// </summary>
+        public static long Squared(this long l)
+        {
+            return l * l;
+        }
+
+        /// <summary>
+        /// Returns the value of this instance taken to Mathf.Floor with .5f added
+        /// </summary>
+        public static float FloorToIntSplit(this float f)
+        {
+            return Mathf.FloorToInt(f) + .5f;
+        }
+
+        /// <summary>
+        /// Returns this float clamped within the Min and Max taking into account the objects scale.
+        /// </summary>
+        public static float ClampWithScale(this float f, Vector2 minMax, float halfScale)
+        {
+            for (int i = 0; i < halfScale; i++)
+            {
+                if (f - i <= minMax.x)
+                {
+                    f = minMax.x + halfScale;
+                    break;
+                }
+                else if (f + i >= minMax.y)
+                {
+                    f = minMax.y - halfScale;
+                    break;
+                }
+            }
+            return f;
+        }
+
+        /// <summary>
+        /// Converts this degrees into radians.
+        /// </summary>
+        public static float DegreesToRadians(this float degrees)
+        {
+            return degrees * (float)(Math.PI / 180.0);
+        }
+
+        /// <summary>
+        /// Returns this euler angle clamped between 0 and 360 degrees.
+        /// </summary>
+        public static float ClampAngle(this float eulerAngle)
+        {
+            eulerAngle -= Mathf.CeilToInt(eulerAngle / 360f) * 360f;
+            if (eulerAngle < 0)
+            {
+                eulerAngle += 360f;
+            }
+            return eulerAngle;
+        }
+
+        /// <summary>
+        /// Returns a value proportional to this value in the range [oldMin, oldMax] when converted to the range [newMin, newMax]
+        /// </summary>
+        public static float ConvertValueToDifferentRange(this float f, float oldMin, float oldMax, float newMin, float newMax)
+        {
+            var oldRange = oldMax - oldMin;
+            if (oldRange == 0)
+            {
+                return newMin;
+            }
+            else
+            {
+                var newRange = newMax - newMin;
+                var newValue = ((f - oldMin) * newRange / oldRange) + newMin;
+                return newValue;
+            }
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/Extensions/VectorExtensions.cs
@@ -0,0 +1,226 @@
+﻿using UnityEngine;
+
+namespace Gogos.Extensions
+{
+    public static class VectorExtensions
+    {
+        /// <summary>
+        /// Returns this Vector2 with a specified X value.
+        /// </summary>
+        public static Vector2 WithX(this Vector2 vec, float x)
+        {
+            vec.x = x;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector2 with a specified Y value.
+        /// </summary>
+        public static Vector2 WithY(this Vector2 vec, float y)
+        {
+            vec.y = y;
+            return vec;
+        }
+
+        /// <summary>
+        /// Flips the X and Y of this Vector2.
+        /// </summary>
+        public static Vector2 Flip(this Vector2 vec)
+        {
+            return new Vector2(vec.y, vec.x);
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with it's X and Z as a new Vector2.
+        /// </summary>
+        public static Vector2 ConvertToVector2(this Vector3 vec)
+        {
+            return new Vector2(vec.x, vec.z);
+        }
+
+        /// <summary>
+        /// Are all of this Vector's values greater than or equal to the specified Vector.
+        /// </summary>
+        public static bool GreaterThanOrEqual(this Vector2 vec1, Vector2 vec2)
+        {
+            if (vec1.x >= vec2.x && vec1.y >= vec2.y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with a specified X value.
+        /// </summary>
+        public static Vector3 WithX(this Vector3 vec, float x)
+        {
+            vec.x = x;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with a specified Y value.
+        /// </summary>
+        public static Vector3 WithY(this Vector3 vec, float y)
+        {
+            vec.y = y;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with a specified Z value.
+        /// </summary>
+        public static Vector3 WithZ(this Vector3 vec, float z)
+        {
+            vec.z = z;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with a specified X and Y value.
+        /// </summary>
+        public static Vector3 WithXY(this Vector3 vec, float x, float y)
+        {
+            vec.x = x;
+            vec.y = y;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with a specified X and Z value.
+        /// </summary>
+        public static Vector3 WithXZ(this Vector3 vec, float x, float z)
+        {
+            vec.x = x;
+            vec.z = z;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with a specified Y and Z value.
+        /// </summary>
+        public static Vector3 WithYZ(this Vector3 vec, float y, float z)
+        {
+            vec.y = y;
+            vec.z = z;
+            return vec;
+        }
+
+        /// <summary>
+        /// Flips the X, Y, and Z of this Vector3.
+        /// </summary>
+        public static Vector3 Flip(this Vector3 vec)
+        {
+            return new Vector3(vec.z, vec.y, vec.x);
+        }
+
+        /// <summary>
+        /// Returns this Vector2 with it's X and Y as a new Vector3's X and Z with a specified Y.
+        /// </summary>
+        public static Vector3 ConvertToVector3(this Vector2 vec, float y = 0)
+        {
+            return new Vector3(vec.x, y, vec.y);
+        }
+
+        /// <summary>
+        /// Are all of this Vector's values greater than or equal to the specified Vector.
+        /// </summary>
+        public static bool GreaterThanOrEqual(this Vector3 vec1, Vector3 vec2)
+        {
+            if (vec1.x >= vec2.x && vec1.y >= vec2.y && vec1.z >= vec2.z)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 rounded to the nearest 1/2.
+        /// </summary>
+        public static Vector3 RoundToNearestHalf(this Vector3 vec)
+        {
+            vec.x = Mathf.RoundToInt(vec.x * 2) / 2f;
+            vec.z = Mathf.RoundToInt(vec.z * 2) / 2f;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 locked to the nearest 1/2.
+        /// *Note: This produces cleaner results than RoundToNearestHalf
+        /// </summary>
+        public static Vector3 LockToNearestHalf(this Vector3 vec)
+        {
+            vec.x = vec.x.FloorToIntSplit();
+            vec.z = vec.z.FloorToIntSplit();
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 rounded to the nearest integer.
+        /// </summary>
+        public static Vector3 RoundToInt(this Vector3 vec)
+        {
+            vec.x = Mathf.RoundToInt(vec.x);
+            vec.y = Mathf.RoundToInt(vec.y);
+            vec.z = Mathf.RoundToInt(vec.z);
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 with it's X and Z rounded to the nearest integer.
+        /// </summary>
+        public static Vector3 RoundToIntXZ(this Vector3 vec)
+        {
+            vec.x = Mathf.RoundToInt(vec.x);
+            vec.z = Mathf.RoundToInt(vec.z);
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector3 clamped within a bounds taking into account the object size.
+        /// </summary>
+        public static Vector3 ClampWithinBounds(this Vector3 vec, Bounds bounds, Vector3 halfScale)
+        {
+            vec.x = vec.x.ClampWithScale(new Vector2(bounds.min.x, bounds.max.x), halfScale.x);
+            vec.z = vec.z.ClampWithScale(new Vector2(bounds.min.z, bounds.max.z), halfScale.z);
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector4 with a specified X value.
+        /// </summary>
+        public static Vector4 WithX(this Vector4 vec, float x)
+        {
+            vec.x = x;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector4 with a specified Y value.
+        /// </summary>
+        public static Vector4 WithY(this Vector4 vec, float y)
+        {
+            vec.y = y;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector4 with a specified Z value.
+        /// </summary>
+        public static Vector4 WithZ(this Vector4 vec, float z)
+        {
+            vec.z = z;
+            return vec;
+        }
+
+        /// <summary>
+        /// Returns this Vector4 with a specified Z value.
+        /// </summary>
+        public static Vector4 WithW(this Vector4 vec, float w)
+        {
+            vec.w = w;
+            return vec;
+        }
+    }
+}
