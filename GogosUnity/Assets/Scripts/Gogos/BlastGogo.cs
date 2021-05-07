e0f919e231c0edd4927c61aa0b99ad1e24dbe96d
PatrickManalich <patrickmanalich@gmail.com>
2021-05-06 23:07:18 -0400

    Create Gogo scripts

---
+++ GogosUnity/Assets/Scripts/Gogos/BlastGogo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class BlastGogo : MonoBehaviour
+    {
+        [SerializeField]
+        private Accelerometer m_Accelerometer;
+
+        [SerializeField]
+        private Animator m_BlastTriggerAnimator;
+
+        private const string ExpandName = "Expand";
+
+        private void Start()
+        {
+            m_Accelerometer.StoppedMoving += Accelerometer_OnStoppedMoving;
+        }
+
+        private void OnDestroy()
+        {
+            m_Accelerometer.StoppedMoving -= Accelerometer_OnStoppedMoving;
+        }
+
+        private void Accelerometer_OnStoppedMoving()
+        {
+            m_BlastTriggerAnimator.SetTrigger(ExpandName);
+
+            m_Accelerometer.StoppedMoving -= Accelerometer_OnStoppedMoving;
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/Gogos/SupportGogo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class SupportGogo : MonoBehaviour
+    {
+        [SerializeField]
+        private Accelerometer m_Accelerometer;
+
+        [SerializeField]
+        private Animator m_SupportTriggerAnimator;
+
+        private const string ExpandName = "Expand";
+
+        private void Start()
+        {
+            m_Accelerometer.StartedMoving += Accelerometer_OnStartedMoving;
+            m_Accelerometer.StoppedMoving += Accelerometer_OnStoppedMoving;
+
+            m_SupportTriggerAnimator.SetBool(ExpandName, true);
+        }
+
+        private void OnDestroy()
+        {
+            m_Accelerometer.StoppedMoving -= Accelerometer_OnStoppedMoving;
+            m_Accelerometer.StartedMoving -= Accelerometer_OnStartedMoving;
+        }
+
+        private void Accelerometer_OnStartedMoving()
+        {
+            m_SupportTriggerAnimator.SetBool(ExpandName, false);
+        }
+
+        private void Accelerometer_OnStoppedMoving()
+        {
+            m_SupportTriggerAnimator.SetBool(ExpandName, true);
+        }
+    }
+}
