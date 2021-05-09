ed9377cfa9dae9d027022c883bc8bda6f12855f7
PatrickManalich <patrickmanalich@gmail.com>
2021-05-08 20:05:55 -0400

    Create TriggerAnimationEventArgs and TriggerStateMachineBehaviour scripts

---
+++ GogosUnity/Assets/Scripts/Triggers/TriggerAnimations/TriggerAnimationEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gogos
+{
+    public enum TriggerAnimation { Invisible, Expand, Expanded }
+
+    public class TriggerAnimationEventArgs : EventArgs
+    {
+        public TriggerAnimation TriggerAnimation { get; }
+
+        public TriggerAnimationEventArgs(TriggerAnimation triggerAnimation)
+        {
+            TriggerAnimation = triggerAnimation;
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/Triggers/TriggerAnimations/TriggerStateMachineBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class TriggerStateMachineBehaviour : StateMachineBehaviour
+    {
+        public event EventHandler<TriggerAnimationEventArgs> AnimationStarted;
+        public event EventHandler<TriggerAnimationEventArgs> AnimationFinished;
+
+        public TriggerAnimation TriggerAnimation => m_TriggerAnimation;
+
+        [SerializeField]
+        private TriggerAnimation m_TriggerAnimation = default;
+
+        private int m_AnimationLoopCount;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_AnimationLoopCount = 0;
+            AnimationStarted?.Invoke(this, new TriggerAnimationEventArgs(m_TriggerAnimation));
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.normalizedTime - m_AnimationLoopCount >= 1)
+            {
+                m_AnimationLoopCount++;
+                AnimationFinished?.Invoke(this, new TriggerAnimationEventArgs(m_TriggerAnimation));
+            }
+        }
+    }
+}
