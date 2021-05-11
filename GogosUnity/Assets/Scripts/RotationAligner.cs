9c4e7ea9f501bfc028eea6c764b86f6a7aa78461
PatrickManalich <patrickmanalich@gmail.com>
2021-05-10 21:04:17 -0400

    Create RotationAligner script

---
+++ GogosUnity/Assets/Scripts/RotationAligner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+	public class RotationAligner : MonoBehaviour
+	{
+		public void AlignWithRotation(Quaternion rotation)
+		{
+			var oldParent = transform.parent;
+
+			transform.localPosition = Vector3.zero;
+			transform.parent = null;
+			transform.rotation = rotation;
+			transform.parent = oldParent;
+		}
+	}
+}
