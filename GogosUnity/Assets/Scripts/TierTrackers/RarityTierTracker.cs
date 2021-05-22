f3f980a7cafebf5cbd07871098b5c35e17310a16
PatrickManalich <patrickmanalich@gmail.com>
2021-05-22 17:57:29 -0400

    Create RarityTierTracker script

---
+++ GogosUnity/Assets/Scripts/TierTrackers/RarityTierTracker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public enum RarityTier { Common, Uncommon, Rare, Golden }
+
+    public class RarityTierTracker : AbstractTierTracker<RarityTier>
+    {
+        public override TierVariant TierVariant => TierVariant.Rarity;
+        public override RarityTier CurrentTier { get => m_CurrentTier; protected set => m_CurrentTier = value; }
+
+        [SerializeField]
+        private RarityTier m_CurrentTier;
+    }
+}
