25bb8c08eb9bc98598e4fcd9ff93ccf8aa995205
PatrickManalich <patrickmanalich@gmail.com>
2021-06-03 21:10:29 -0400

    Create PointValueTierTracker script

---
+++ GogosUnity/Assets/Scripts/TierTrackers/PointValueTierTracker.cs
@@ -0,0 +1,9 @@
+﻿namespace Gogos
+{
+    public enum PointValueTier { Low, Medium, High, Immense }
+
+    public class PointValueTierTracker : AbstractTierTracker<PointValueTier>
+    {
+        public override TierVariant TierVariant => TierVariant.PointValue;
+    }
+}
