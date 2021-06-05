64a198b759af05a9493c706ba5b0ab2741960329
PatrickManalich <patrickmanalich@gmail.com>
2021-06-05 11:00:05 -0400

    Create TierTrackerReference script

---
+++ GogosUnity/Assets/Scripts/TierTrackers/TierTrackerReference.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gogos
+{
+	public class TierTrackerReference : MonoBehaviour
+	{
+        public IReadOnlyList<AbstractTierTracker> TierTrackers { get; private set; }
+
+        private Dictionary<TierVariant, AbstractTierTracker> m_TierTrackersByVariant = new Dictionary<TierVariant, AbstractTierTracker>();
+
+        private void Awake()
+        {
+            TierTrackers = GetComponents<AbstractTierTracker>();
+            foreach (var tierTracker in TierTrackers)
+            {
+                m_TierTrackersByVariant.Add(tierTracker.TierVariant, tierTracker);
+            }
+        }
+
+        public AbstractTierTracker GetTierTrackerForVariant(TierVariant tierVariant)
+        {
+            return m_TierTrackersByVariant[tierVariant];
+        }
+	}
+}
