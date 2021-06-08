9a426a76e6b4de1bac64ef11c7798ce05f970034
PatrickManalich <patrickmanalich@gmail.com>
2021-06-07 21:45:04 -0400

    Create Player and PlayerManager scripts

---
+++ GogosUnity/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gogos
+{
+	public class PlayerManager : MonoBehaviour
+	{
+        [SerializeField]
+        private ScriptableGogoBucket m_Starters;
+
+		private const int PlayerCount = 3;
+
+		private Player[] m_Players = new Player[PlayerCount];
+
+        private void Awake()
+		{
+			var playerColors = Enum.GetValues(typeof(PlayerColor));
+			for (int i = 0; i < PlayerCount; i++)
+            {
+				var playerName = $"Player{i + 1}";
+				var playerColor = (PlayerColor)playerColors.GetValue(i);
+				var player = new Player(playerName, playerColor);
+				player.AddToCollection(m_Starters.ScriptableGogos);
+				m_Players[i] = player;
+            }
+		}
+	}
+}
---
+++ GogosUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Gogos
+{
+	public enum PlayerColor { Red, Green, Blue }
+
+	public class Player
+	{
+		public string Name { get; }
+
+		public PlayerColor PlayerColor { get; }
+
+		public List<ScriptableGogo> Collection { get; private set; } = new List<ScriptableGogo>();
+
+		public Player(string name, PlayerColor playerColor)
+        {
+			Name = name;
+			PlayerColor = playerColor;
+        }
+
+		public void AddToCollection(IEnumerable<ScriptableGogo> scriptableGogos)
+        {
+			Collection.AddRange(scriptableGogos);
+        }
+	}
+}
