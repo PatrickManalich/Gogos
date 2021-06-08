9c8dfe20119d190ce66ef389474bdcf59e75d8b7
PatrickManalich <patrickmanalich@gmail.com>
2021-06-07 21:43:13 -0400

    Create ScriptableGogoBucket script

---
+++ GogosUnity/Assets/Scripts/ScriptableObjects/ScriptableGogoBucket.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    [CreateAssetMenu(fileName = "ScriptableGogoBucket", menuName = "Gogos/Scriptable Gogo Bucket")]
+    public class ScriptableGogoBucket : ScriptableObject
+	{
+        [SerializeField]
+        private ScriptableGogo[] m_ScriptableGogos;
+        public ScriptableGogo[] ScriptableGogos => m_ScriptableGogos;
+    }
+}
