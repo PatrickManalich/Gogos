0a9c9a1b9d928d19942c4bc70c76c4e89b7fab41
PatrickManalich <patrickmanalich@gmail.com>
2021-06-11 21:53:52 -0400

    Create HoldDownButton and GogoLauncherButtonInputs scripts

---
+++ GogosUnity/Assets/Scripts/UserInterface/GogoLauncherButtonInputs.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gogos
+{
+	public class GogoLauncherButtonInputs : MonoBehaviour
+	{
+		[SerializeField]
+		private GogoLauncher m_GogoLauncher;
+
+        [SerializeField]
+        private HoldDownButton m_MoveLeftButton;
+
+		[SerializeField]
+		private HoldDownButton m_MoveRightButton;
+
+		[SerializeField]
+		private HoldDownButton m_DecreaseLaunchForceButton;
+
+		[SerializeField]
+		private HoldDownButton m_IncreaseLaunchForceButton;
+
+        [SerializeField]
+        private Button m_LaunchButton;
+
+        private void Start()
+		{
+			m_MoveLeftButton.Held += m_GogoLauncher.MoveLeft;
+			m_MoveRightButton.Held += m_GogoLauncher.MoveRight;
+			m_DecreaseLaunchForceButton.Held += m_GogoLauncher.DecreaseLaunchForce;
+			m_IncreaseLaunchForceButton.Held += m_GogoLauncher.IncreaseLaunchForce;
+			m_LaunchButton.onClick.AddListener(LaunchButton_OnClick);
+		}
+
+        private void OnDestroy()
+		{
+			m_LaunchButton.onClick.RemoveListener(LaunchButton_OnClick);
+			m_IncreaseLaunchForceButton.Held -= m_GogoLauncher.IncreaseLaunchForce;
+			m_DecreaseLaunchForceButton.Held -= m_GogoLauncher.DecreaseLaunchForce;
+			m_MoveRightButton.Held -= m_GogoLauncher.MoveRight;
+			m_MoveLeftButton.Held -= m_GogoLauncher.MoveLeft;
+		}
+
+		private void LaunchButton_OnClick()
+        {
+            if (m_GogoLauncher.ReadyForLaunch)
+            {
+				m_GogoLauncher.Launch();
+			}
+        }
+	}
+}
---
+++ GogosUnity/Assets/Scripts/UserInterface/HoldDownButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Gogos
+{
+    public class HoldDownButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public event Action Held;
+        private Coroutine m_EndlessHoldCoroutine;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            StopEndlessHold();
+            StartEndlessHold();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            StopEndlessHold();
+        }
+
+        private void StartEndlessHold()
+        {
+            m_EndlessHoldCoroutine = StartCoroutine(EndlessHold());
+        }
+
+        private void StopEndlessHold()
+        {
+            if (m_EndlessHoldCoroutine != null)
+            {
+                StopCoroutine(m_EndlessHoldCoroutine);
+                m_EndlessHoldCoroutine = null;
+            }
+        }
+
+        private IEnumerator EndlessHold()
+        {
+            while (true)
+            {
+                Held?.Invoke();
+                yield return null;
+            }
+        }
+    }
+}
