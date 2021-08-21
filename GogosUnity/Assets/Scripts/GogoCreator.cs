f8e82e45ae96bc1768eef65850e6ddb5fa234fc7
PatrickManalich <patrickmanalich@gmail.com>
2021-08-21 11:50:17 -0400

    Create GogoCreator script

---
+++ GogosUnity/Assets/Scripts/GogoCreator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+	public class GogoCreator : MonoBehaviour
+	{
+        [SerializeField]
+        private Launcher m_Launcher;
+
+        private AbstractGogo m_CreatedGogo;
+
+		public void CreateGogo(AbstractScriptableGogo scriptableGogo)
+        {
+			if (m_CreatedGogo != null)
+            {
+				Destroy(m_CreatedGogo.gameObject);
+            }
+
+			m_CreatedGogo = Instantiate(scriptableGogo.Prefab).GetComponent<AbstractGogo>();
+			m_CreatedGogo.SetTiers(scriptableGogo);
+			m_CreatedGogo.name = m_CreatedGogo.name.Replace("(Clone)", "");
+
+			m_Launcher.LoadProjectile(m_CreatedGogo.gameObject);
+        }
+	}
+}
