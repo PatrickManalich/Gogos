ab2180fccff145515524cbc569da71a1a876196c
PatrickManalich <patrickmanalich@gmail.com>
2021-08-24 21:13:28 -0400

    Create PhaseToggler script

---
+++ GogosUnity/Assets/Scripts/PhaseToggler.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Gogos
+{
+	public class PhaseToggler : MonoBehaviour
+	{
+        [SerializeField]
+        private PhaseTracker m_PhaseTracker;
+
+        [SerializeField]
+        private GameObject[] m_ItemsToToggle;
+
+        [SerializeField]
+        private Phase[] m_ActiveForPhases;
+
+        private void Start()
+		{
+            m_PhaseTracker.PhaseChanged += ToggleBasedOnPhase;
+
+            ToggleBasedOnPhase();
+		}
+
+        private void OnDestroy()
+		{
+            m_PhaseTracker.PhaseChanged -= ToggleBasedOnPhase;
+        }
+
+        private void ToggleBasedOnPhase()
+        {
+            var active = m_ActiveForPhases.Contains(m_PhaseTracker.Phase);
+            foreach (var item in m_ItemsToToggle)
+            {
+                item.SetActive(active);
+            }
+        }
+    }
+}
