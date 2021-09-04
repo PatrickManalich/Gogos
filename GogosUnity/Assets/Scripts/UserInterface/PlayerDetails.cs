18e6cc90bd7f13fa3ba6c6a40874b0b50fd1d2d7
PatrickManalich <patrickmanalich@gmail.com>
2021-09-04 11:29:17 -0400

    Create player details scripts

---
+++ GogosUnity/Assets/Scripts/UserInterface/PlayerDetails.cs
@@ -0,0 +1,28 @@
+﻿using RotaryHeart.Lib.SerializableDictionaryPro;
+using TMPro;
+using UnityEngine;
+
+namespace Gogos
+{
+	public class PlayerDetails : MonoBehaviour
+	{
+        [System.Serializable]
+        private class ColorsByPlayerColor : SerializableDictionary<PlayerColor, Color> { }
+
+        [SerializeField]
+        private TextMeshProUGUI m_NameText;
+
+        [SerializeField]
+        private ColorsByPlayerColor m_ColorsByPlayerColor;
+
+        [SerializeField]
+        private GameObject m_SelectedIndicator;
+
+		public void SetDetails(Player player)
+        {
+            m_NameText.text = player.Name;
+            m_NameText.color = m_ColorsByPlayerColor[player.PlayerColor];
+            m_SelectedIndicator.SetActive(player == GameManager.PlayerManager.CurrentPlayer);
+        }
+	}
+}
---
+++ GogosUnity/Assets/Scripts/UserInterface/PlayerDetailsCreator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+	public class PlayerDetailsCreator : MonoBehaviour
+	{
+        [SerializeField]
+        private GameObject m_PlayerDetailsPrefab;
+
+        private void Start()
+		{
+			foreach (Transform child in transform)
+			{
+				Destroy(child.gameObject);
+			}
+
+			foreach (var player in GameManager.PlayerManager.Players)
+            {
+				var playerDetails = Instantiate(m_PlayerDetailsPrefab, transform).GetComponent<PlayerDetails>();
+				playerDetails.SetDetails(player);
+				playerDetails.name = playerDetails.name.Replace("(Clone)", player.Name);
+			}
+		}
+	}
+}
