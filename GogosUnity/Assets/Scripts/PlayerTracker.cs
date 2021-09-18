84c7d4c64217e44d8af9ca609c7146fff4c8416e
PatrickManalich <patrickmanalich@gmail.com>
2021-09-18 18:03:51 -0400

    Create PlayerTracker script

---
+++ GogosUnity/Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gogos
+{
+	public class PlayerTracker : MonoBehaviour
+	{
+		public Player Player => Players[m_PlayerIndex];
+
+		public Player[] Players { get; private set; } = new Player[PlayerCount];
+
+		[SerializeField]
+		private ScriptableGogoBucket m_Starters;
+
+		private const int PlayerCount = 3;
+
+		private int m_PlayerIndex;
+
+		private void Awake()
+		{
+			var playerColors = Enum.GetValues(typeof(PlayerColor));
+			for (int i = 0; i < PlayerCount; i++)
+			{
+				var playerName = $"Player{i + 1}";
+				var playerColor = (PlayerColor)playerColors.GetValue(i);
+				var player = new Player(playerName, playerColor);
+				player.AddToCollection(m_Starters.ScriptableGogos);
+				Players[i] = player;
+			}
+		}
+	}
+}
