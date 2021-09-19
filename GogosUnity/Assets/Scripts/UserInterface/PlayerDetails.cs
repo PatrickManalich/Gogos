dded7c182ec77b6e53b6cecf5eaf3582f4bf9f34
PatrickManalich <patrickmanalich@gmail.com>
2021-09-18 20:14:29 -0400

    Update selected indicator on player change

--- GogosUnity/Assets/Scripts/UserInterface/PlayerDetails.cs
+++ GogosUnity/Assets/Scripts/UserInterface/PlayerDetails.cs
@@ -19,17 +19,34 @@ namespace Gogos
         private GameObject m_SelectedIndicator;
 
         private PlayerTracker m_PlayerTracker;
+        private Player m_Player;
 
         private void Awake()
         {
             m_PlayerTracker = FindObjectOfType<PlayerTracker>();
         }
 
+        private void Start()
+        {
+            m_PlayerTracker.PlayerChanged += PlayerTracker_OnPlayerChanged;
+        }
+
+        private void OnDestroy()
+        {
+            m_PlayerTracker.PlayerChanged -= PlayerTracker_OnPlayerChanged;
+        }
+
         public void SetDetails(Player player)
         {
-            m_NameText.text = player.Name;
-            m_NameText.color = m_ColorsByPlayerColor[player.PlayerColor];
-            m_SelectedIndicator.SetActive(player == m_PlayerTracker.Player);
+            m_Player = player;
+            m_NameText.text = m_Player.Name;
+            m_NameText.color = m_ColorsByPlayerColor[m_Player.PlayerColor];
+            m_SelectedIndicator.SetActive(m_Player == m_PlayerTracker.Player);
+        }
+
+        private void PlayerTracker_OnPlayerChanged()
+        {
+            m_SelectedIndicator.SetActive(m_Player == m_PlayerTracker.Player);
         }
     }
 }
