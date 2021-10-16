f4c76af0f3821bd9e3ef5d19f40a38ebf3b25c82
PatrickManalich <patrickmanalich@gmail.com>
2021-10-16 18:17:42 -0400

    Create AbstractSingleton script

---
+++ GogosUnity/Assets/Scripts/AbstractSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public abstract class AbstractSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
