a5d3bbe34647adb8e3a9044440f23b935697c9c0
PatrickManalich <patrickmanalich@gmail.com>
2021-10-20 23:02:19 -0400

    Track points in Player script

--- GogosUnity/Assets/Scripts/Player.cs
+++ GogosUnity/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Gogos
 {
@@ -10,12 +11,19 @@ namespace Gogos
 
         public PlayerColor PlayerColor { get; }
 
+        public int Points { get; private set; }
+
         public List<AbstractScriptableGogo> Collection { get; } = new List<AbstractScriptableGogo>();
 
         public Player(string name, PlayerColor playerColor)
         {
             Name = name;
             PlayerColor = playerColor;
+        }
+
+        public void AddPoints(int points)
+        {
+            Points += Mathf.Max(0, points);
         }
     }
 }
