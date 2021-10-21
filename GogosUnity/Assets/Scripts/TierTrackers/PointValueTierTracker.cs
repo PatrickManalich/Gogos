efde4175df8a2df087ef9d2d8ecd1229ff2878f4
PatrickManalich <patrickmanalich@gmail.com>
2021-10-20 23:03:36 -0400

    Assign point values in PointValueTierTracker script

--- GogosUnity/Assets/Scripts/TierTrackers/PointValueTierTracker.cs
+++ GogosUnity/Assets/Scripts/TierTrackers/PointValueTierTracker.cs
@@ -1,9 +1,21 @@
-﻿namespace Gogos
+﻿using System.Collections.Generic;
+
+namespace Gogos
 {
     public enum PointValueTier { Low, Medium, High, Immense }
 
     public class PointValueTierTracker : AbstractTierTracker<PointValueTier>
     {
         public override TierVariant TierVariant => TierVariant.PointValue;
+
+        public int PointValue => PointValuesByTier[Tier];
+
+        private static readonly Dictionary<PointValueTier, int> PointValuesByTier = new Dictionary<PointValueTier, int>()
+        {
+            { PointValueTier.Low, 100 },
+            { PointValueTier.Medium, 200 },
+            { PointValueTier.High, 400 },
+            { PointValueTier.Immense, 1000 },
+        };
     }
 }
