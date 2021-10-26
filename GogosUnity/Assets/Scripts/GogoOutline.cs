711fda74fabbf885f9ca44bd5ab30c4f1efde71e
PatrickManalich <patrickmanalich@gmail.com>
2021-10-25 21:04:58 -0400

    Create GogoOutline script

---
+++ GogosUnity/Assets/Scripts/GogoOutline.cs
@@ -0,0 +1,12 @@
+﻿using cakeslice;
+
+namespace Gogos
+{
+    public class GogoOutline : Outline
+    {
+        private void Start()
+        {
+            color = (int)PlayerTracker.Player.PlayerColor;
+        }
+    }
+}
