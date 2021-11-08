32321013f73a407c5d90a5f2f27e1a448ae2d65c
PatrickManalich <patrickmanalich@gmail.com>
2021-11-08 18:45:13 -0500

    Create IdentifiableGogo and Collection scripts

---
+++ GogosUnity/Assets/Scripts/Collection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gogos
+{
+    public class Collection
+    {
+        public event EventHandler<IdentifiableGogoEventArgs> GogoAdded;
+
+        public IReadOnlyList<IdentifiableGogo> IdentifiableGogos => m_IdentifiableGogosById.Values.ToList();
+
+        private Dictionary<string, IdentifiableGogo> m_IdentifiableGogosById = new Dictionary<string, IdentifiableGogo>();
+
+        public void Add(IEnumerable<AbstractScriptableGogo> scriptableGogos)
+        {
+            foreach (var scriptableGogo in scriptableGogos)
+            {
+                Add(scriptableGogo);
+            }
+        }
+
+        public void Add(AbstractScriptableGogo scriptableGogo)
+        {
+            var identifiableGogo = new IdentifiableGogo(scriptableGogo);
+            m_IdentifiableGogosById.Add(identifiableGogo.Id, identifiableGogo);
+            GogoAdded?.Invoke(this, new IdentifiableGogoEventArgs(identifiableGogo));
+        }
+
+        public IdentifiableGogo Get(string id)
+        {
+            return m_IdentifiableGogosById[id];
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/IdentifiableGogo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gogos
+{
+    public class IdentifiableGogo
+    {
+        public string Id { get; set; }
+        public AbstractScriptableGogo ScriptableGogo { get; }
+
+        public IdentifiableGogo(AbstractScriptableGogo scriptableGogo)
+        {
+            Id = Guid.NewGuid().ToString();
+            ScriptableGogo = scriptableGogo;
+        }
+    }
+}
