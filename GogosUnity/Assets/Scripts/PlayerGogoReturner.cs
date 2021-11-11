b74955749d350c8b82f06fad0de470e21f842473
PatrickManalich <patrickmanalich@gmail.com>
2021-11-10 22:22:49 -0500

    Create PlayerGogoReturner script

---
+++ GogosUnity/Assets/Scripts/PlayerGogoReturner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class PlayerGogoReturner : MonoBehaviour
+    {
+        public event Action Returned;
+
+        [SerializeField]
+        private TextMeshProUGUI m_ReturningText;
+
+        private void Start()
+        {
+            PhaseTracker.PhaseChanged += PhaseTracker_OnPhaseChanged;
+        }
+
+        private void OnDestroy()
+        {
+            PhaseTracker.PhaseChanged -= PhaseTracker_OnPhaseChanged;
+        }
+
+        private void PhaseTracker_OnPhaseChanged()
+        {
+            if (PhaseTracker.Phase == Phase.Returning)
+            {
+                StartCoroutine(FlashTextAndReturn());
+            }
+        }
+
+        private IEnumerator FlashTextAndReturn()
+        {
+            var identifiableGogos = PlayerTracker.Player.Collection.IdentifiableGogos;
+            if (!identifiableGogos.Any(i => GogoSituationDatabase.Instance.GetSituation(i) == Situation.Available))
+            {
+                m_ReturningText.gameObject.SetActive(true);
+                yield return new WaitForSeconds(1);
+
+                var playersGogos = FindObjectsOfType<AbstractGogo>().Where(g => g.Player == PlayerTracker.Player);
+                foreach (var playerGogo in playersGogos)
+                {
+                    Destroy(playerGogo.gameObject);
+                }
+
+                foreach (var identifiableGogo in identifiableGogos)
+                {
+                    GogoSituationDatabase.Instance.SetSituation(identifiableGogo, Situation.Available);
+                }
+                yield return new WaitForSeconds(1);
+
+                m_ReturningText.gameObject.SetActive(false);
+            }
+            Returned?.Invoke();
+        }
+    }
+}
