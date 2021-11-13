b429f3c6d4184481f25d7b29c64a73ecc3c88b67
PatrickManalich <patrickmanalich@gmail.com>
2021-11-13 09:26:54 -0500

    Create TurnTracker script

---
+++ GogosUnity/Assets/Scripts/Singletons/TurnTracker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Gogos
+{
+    public class TurnTracker : AbstractSingleton<TurnTracker>
+    {
+        public static event Action TurnChanged;
+
+        public static int Turn { get; private set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Turn = 0;
+        }
+
+        private void Start()
+        {
+            PlayerTracker.PlayerChanged += PlayerTracker_OnPlayerChanged;
+        }
+
+        private void OnDestroy()
+        {
+            PlayerTracker.PlayerChanged -= PlayerTracker_OnPlayerChanged;
+        }
+
+        private void PlayerTracker_OnPlayerChanged()
+        {
+            Turn++;
+            TurnChanged?.Invoke();
+        }
+    }
+}
