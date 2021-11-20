40c861df43580e8413ce49ef7d5c0be2fd9419a1
PatrickManalich <patrickmanalich@gmail.com>
2021-11-20 06:09:02 -0500

    Create LaunchedGogoObserver script

---
+++ GogosUnity/Assets/Scripts/Launching/LaunchedGogoObserver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class LaunchedGogoObserver : MonoBehaviour, ITriggerAnimationObserver
+    {
+        public event Action Expanded;
+
+        [SerializeField]
+        private Launcher m_Launcher;
+
+        private TriggerAnimationSubject m_TriggerAnimationSubject;
+
+        private void Start()
+        {
+            PhaseTracker.PhaseChanged += PhaseTracker_OnPhaseChanged;
+        }
+
+        private void OnDestroy()
+        {
+            PhaseTracker.PhaseChanged -= PhaseTracker_OnPhaseChanged;
+        }
+
+        public void Notify()
+        {
+            m_TriggerAnimationSubject.RemoveObserverForAnimationFinished(this, TriggerAnimation.Expand);
+            m_TriggerAnimationSubject = null;
+            Expanded?.Invoke();
+        }
+
+        private void PhaseTracker_OnPhaseChanged()
+        {
+            if (PhaseTracker.Phase == Phase.Launching)
+            {
+                m_TriggerAnimationSubject = m_Launcher.Projectile.GetComponentInChildren<TriggerAnimationSubject>();
+                m_TriggerAnimationSubject.AddObserverForAnimationFinished(this, TriggerAnimation.Expand);
+            }
+        }
+    }
+}
