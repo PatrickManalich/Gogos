4afb05341967bf64dea0e6cb385347024f55110d
PatrickManalich <patrickmanalich@gmail.com>
2021-12-11 18:32:47 -0500

    [refactor] Use TierSlot in TierDetails script

--- GogosUnity/Assets/Scripts/UserInterface/TierDetails.cs
+++ GogosUnity/Assets/Scripts/UserInterface/TierDetails.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Gogos
 {
     public class TierDetails : MonoBehaviour
     {
         [SerializeField]
-        private Image[] m_TierSlots;
-
-        [SerializeField]
-        private Color m_FilledColor;
-
-        [SerializeField]
-        private Color m_EmptyColor;
+        private TierSlot[] m_TierSlots;
 
         public void SetSlots<T>(T tier) where T : Enum
         {
@@ -21,7 +14,14 @@ namespace Gogos
             for (int i = 0; i < m_TierSlots.Length; i++)
             {
                 var tierSlot = m_TierSlots[i];
-                tierSlot.color = i < slotsFilled ? m_FilledColor : m_EmptyColor;
+                if (i < slotsFilled)
+                {
+                    tierSlot.Fill();
+                }
+                else
+                {
+                    tierSlot.Empty();
+                }
             }
         }
     }
