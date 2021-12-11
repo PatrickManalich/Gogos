796a84816d680ce22e3fe280122992f4f56e3dfb
PatrickManalich <patrickmanalich@gmail.com>
2021-12-11 16:40:41 -0500

    Create ScriptableColorPalette script

---
+++ GogosUnity/Assets/Scripts/ScriptableObjects/ScriptableColorPalette.cs
@@ -0,0 +1,24 @@
+﻿using RotaryHeart.Lib.SerializableDictionaryPro;
+using UnityEngine;
+
+namespace Gogos
+{
+    [CreateAssetMenu(fileName = "ColorPalette", menuName = "Gogos/Scriptable Color Palette")]
+    public class ScriptableColorPalette : ScriptableObject
+    {
+        [System.Serializable]
+        private class ColorsByPlayerColor : SerializableDictionary<PlayerColor, Color> { }
+
+        [SerializeField]
+        private ColorsByPlayerColor m_ColorsByPlayerColor;
+
+        [SerializeField]
+        private Color m_Grey;
+        public Color Grey => m_Grey;
+
+        public Color GetColorForPlayerColor(PlayerColor playerColor)
+        {
+            return m_ColorsByPlayerColor[playerColor];
+        }
+    }
+}
