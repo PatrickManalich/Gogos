7c4f0190847e8f1909d1af20fb74d88b8d9fad33
PatrickManalich <patrickmanalich@gmail.com>
2021-12-11 18:32:25 -0500

    Create TierSlot script

---
+++ GogosUnity/Assets/Scripts/UserInterface/TierSlot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gogos
+{
+    public class TierSlot : MonoBehaviour
+    {
+        [SerializeField]
+        private Image m_Image;
+
+        [SerializeField]
+        private Color m_FilledColor;
+
+        [SerializeField]
+        private Color m_EmptyColor;
+
+        public void Fill()
+        {
+            m_Image.color = m_FilledColor;
+        }
+
+        public void Empty()
+        {
+            m_Image.color = m_EmptyColor;
+        }
+    }
+}
