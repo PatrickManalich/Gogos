21ddcce24389fee27d5693015c832eb7b804217c
PatrickManalich <patrickmanalich@gmail.com>
2021-12-12 09:56:47 -0500

    Allow icons to be toggled in TierSlot script

--- GogosUnity/Assets/Scripts/UserInterface/TierSlot.cs
+++ GogosUnity/Assets/Scripts/UserInterface/TierSlot.cs
@@ -9,6 +9,12 @@ namespace Gogos
         private Image m_Image;
 
         [SerializeField]
+        private Image m_PlusIcon;
+
+        [SerializeField]
+        private Image m_MinusIcon;
+
+        [SerializeField]
         private Color m_FilledColor;
 
         [SerializeField]
@@ -22,6 +28,22 @@ namespace Gogos
         public void Empty()
         {
             m_Image.color = m_EmptyColor;
+        }
+
+        public void ShowPlusIcon()
+        {
+            m_PlusIcon.gameObject.SetActive(true);
+        }
+
+        public void ShowMinusIcon()
+        {
+            m_MinusIcon.gameObject.SetActive(true);
+        }
+
+        public void HideIcons()
+        {
+            m_PlusIcon.gameObject.SetActive(false);
+            m_MinusIcon.gameObject.SetActive(false);
         }
     }
 }
