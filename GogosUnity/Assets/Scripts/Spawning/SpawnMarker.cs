878f9062eecb2c1183a8dba5e122b80fe86c6fda
PatrickManalich <patrickmanalich@gmail.com>
2021-12-12 11:01:20 -0500

    Use snap on enable in SpawnMarker script

--- GogosUnity/Assets/Scripts/Spawning/SpawnMarker.cs
+++ GogosUnity/Assets/Scripts/Spawning/SpawnMarker.cs
@@ -4,12 +4,8 @@ namespace Gogos
 {
     public class SpawnMarker : MonoBehaviour
     {
-        [SerializeField]
-        private GroundSnapper m_GroundSnapper;
-
         public void MarkWithRadius(float radius)
         {
-            m_GroundSnapper.SnapToGround();
             transform.localScale = 2 * radius * Vector3.one;
         }
     }
