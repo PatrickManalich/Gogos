c5c6e615be6e14df18f6725eb6b2dd8ed9afa895
PatrickManalich <patrickmanalich@gmail.com>
2021-12-12 10:08:36 -0500

    Create StringExtensions script

---
+++ GogosUnity/Assets/Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Gogos
+{
+    public static class StringExtensions
+    {
+        public static string SplitOnCamelCase(this string s)
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i > 0 && char.IsUpper(s[i]))
+                {
+                    stringBuilder.Append(' ');
+                }
+                stringBuilder.Append(s[i]);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
