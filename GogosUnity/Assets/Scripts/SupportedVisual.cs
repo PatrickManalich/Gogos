e4867853dc5781b826bc54ef12a004fa862af32c
PatrickManalich <patrickmanalich@gmail.com>
2021-12-15 18:39:24 -0500

    Create SupportedVisual script

---
+++ GogosUnity/Assets/Scripts/SupportedVisual.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class SupportedVisual : MonoBehaviour
+    {
+        [SerializeField]
+        private Accelerometer m_Accelerometer;
+
+        [SerializeField]
+        private SupportableAttribute m_SupportableAttribute;
+
+        [SerializeField]
+        private GameObject m_Visual;
+
+        private void Update()
+        {
+            m_Visual.SetActive(m_SupportableAttribute.IsSupported && !m_Accelerometer.IsMoving);
+        }
+    }
+}
