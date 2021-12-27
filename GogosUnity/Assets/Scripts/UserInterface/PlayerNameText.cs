dae834fd35d06df2dad31191d190ef509d3579be
PatrickManalich <patrickmanalich@gmail.com>
2021-12-26 21:59:05 -0500

    Create PlayerNameText script

---
+++ GogosUnity/Assets/Scripts/UserInterface/PlayerNameText.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class PlayerNameText : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI m_Text;
+
+        [SerializeField]
+        private ScriptableColorPalette m_ScriptableColorPalette;
+
+        public void SetPlayerName(Player player)
+        {
+            var palette = m_ScriptableColorPalette;
+            var isUnclaimed = player == null;
+            m_Text.text = isUnclaimed ? "Unclaimed" : player.Name;
+            m_Text.color = isUnclaimed ? palette.Grey : palette.GetColorForPlayerColor(player.PlayerColor);
+        }
+    }
+}
