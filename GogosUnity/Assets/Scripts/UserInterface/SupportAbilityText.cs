7777639aae7119840566064b5a8df09ff8e0531c
PatrickManalich <patrickmanalich@gmail.com>
2021-12-26 22:05:30 -0500

    Create SupportAbilityText script

---
+++ GogosUnity/Assets/Scripts/UserInterface/SupportAbilityText.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class SupportAbilityText : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI m_Text;
+
+        public void SetSupportAbility(TierVariant tierVariant, int tierModifier)
+        {
+            var tierVariantText = tierVariant.ToString().SplitOnCamelCase();
+            var tierModifierText = tierModifier.ToString("+0;-#");
+            m_Text.text = $"Ally Gogo {tierVariantText} {tierModifierText}";
+        }
+    }
+}
