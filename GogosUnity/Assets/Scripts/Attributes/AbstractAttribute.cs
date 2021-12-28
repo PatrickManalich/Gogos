68815290f2d82b54e0102372b7910279f89b24bc
PatrickManalich <patrickmanalich@gmail.com>
2021-12-27 21:40:37 -0500

    Create AbstractAttribute script

---
+++ GogosUnity/Assets/Scripts/Attributes/AbstractAttribute.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public abstract class AbstractAttribute : MonoBehaviour
+    {
+        public Player Player { get; set; }
+
+        protected abstract void OnTriggerEntered(TriggerEventArgs e);
+
+        protected abstract void OnTriggerExited(TriggerEventArgs e);
+
+        [SerializeField]
+        protected TriggerListener m_TriggerListener;
+
+        private void Awake()
+        {
+            m_TriggerListener.Entered += TriggerListener_OnEntered;
+            m_TriggerListener.Exited += TriggerListener_OnExited;
+        }
+
+        private void OnDestroy()
+        {
+            m_TriggerListener.Exited -= TriggerListener_OnExited;
+            m_TriggerListener.Entered -= TriggerListener_OnEntered;
+        }
+
+        private void TriggerListener_OnEntered(object sender, TriggerEventArgs e)
+        {
+            OnTriggerEntered(e);
+        }
+
+        private void TriggerListener_OnExited(object sender, TriggerEventArgs e)
+        {
+            OnTriggerExited(e);
+        }
+    }
+}
