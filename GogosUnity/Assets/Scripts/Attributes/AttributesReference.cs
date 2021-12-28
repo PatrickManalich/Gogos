661ba01f2f29e91a23c01751e069ad12f59600be
PatrickManalich <patrickmanalich@gmail.com>
2021-12-27 21:51:37 -0500

    Create AttributesReference script

---
+++ GogosUnity/Assets/Scripts/Attributes/AttributesReference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class AttributesReference : MonoBehaviour
+    {
+        public IReadOnlyList<AbstractAttribute> Attributes { get; private set; }
+
+        private void Awake()
+        {
+            Attributes = GetComponents<AbstractAttribute>();
+        }
+    }
+}
