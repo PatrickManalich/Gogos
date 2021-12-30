1aca26a015ecde46c37674094f94ffd51dfd3565
PatrickManalich <patrickmanalich@gmail.com>
2021-12-29 22:48:12 -0500

    Create blasted effect scripts

---
+++ GogosUnity/Assets/Scripts/Effects/AbstractBlastedEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public abstract class AbstractBlastedEffect : MonoBehaviour
+    {
+        protected abstract void OnBlasted();
+
+        [SerializeField]
+        private BlastableAttribute m_BlastableAttribute;
+
+        private void Start()
+        {
+            m_BlastableAttribute.Blasted += BlastableAttribute_OnBlasted;
+        }
+
+        private void OnDestroy()
+        {
+            m_BlastableAttribute.Blasted -= BlastableAttribute_OnBlasted;
+        }
+
+        private void BlastableAttribute_OnBlasted()
+        {
+            OnBlasted();
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/Effects/BombBlastedEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class BombBlastedEffect : AbstractBlastedEffect, ITriggerAnimationObserver
+    {
+        [SerializeField]
+        private Rigidbody m_Rigidbody;
+
+        [SerializeField]
+        private GameObject m_Visuals;
+
+        [SerializeField]
+        private TriggerRangeRefresher m_TriggerRangeRefresher;
+
+        [SerializeField]
+        private BlastForceTierTracker m_BlastForceTierTracker;
+
+        [SerializeField]
+        private BlastTrigger m_BlastTrigger;
+
+        [SerializeField]
+        private TriggerAnimationSubject m_TriggerAnimationSubject;
+
+        public void Notify()
+        {
+            m_TriggerAnimationSubject.RemoveObserverForAnimationFinished(this, TriggerAnimation.Expand);
+            Destroy(m_Rigidbody.gameObject);
+        }
+
+        protected override void OnBlasted()
+        {
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            m_Visuals.SetActive(false);
+            m_TriggerRangeRefresher.enabled = false;
+
+            m_TriggerAnimationSubject.AddObserverForAnimationFinished(this, TriggerAnimation.Expand);
+            m_BlastTrigger.Blast(m_BlastForceTierTracker);
+        }
+    }
+}
