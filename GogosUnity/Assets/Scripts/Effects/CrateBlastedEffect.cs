e411081a934151a274f69c4ba343816fe4e32b1e
PatrickManalich <patrickmanalich@gmail.com>
2021-12-30 20:49:37 -0500

    Create crate scripts

---
+++ GogosUnity/Assets/Scripts/Effects/CrateBlastedEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class CrateBlastedEffect : AbstractBlastedEffect
+    {
+        [SerializeField]
+        private GameObject m_Root;
+
+        [SerializeField]
+        private CapacityTierTracker m_CapacityTierTracker;
+
+        [SerializeField]
+        private GameObject[] m_PointCubes;
+
+        protected override void OnBlasted()
+        {
+            for (int i = 0; i < m_CapacityTierTracker.Capacity; i++)
+            {
+                var pointCube = m_PointCubes[i];
+                pointCube.SetActive(true);
+                pointCube.transform.parent = null;
+            }
+            Destroy(m_Root);
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/TierTrackers/CapacityTierTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Gogos
+{
+    public enum CapacityTier { Low, Medium, High }
+
+    public class CapacityTierTracker : AbstractTierTracker<CapacityTier>
+    {
+        public override TierVariant TierVariant => TierVariant.Capacity;
+
+        public int Capacity => CapacitiesByTier[Tier];
+
+        private static readonly Dictionary<CapacityTier, int> CapacitiesByTier = new Dictionary<CapacityTier, int>()
+        {
+            { CapacityTier.Low, 4 },
+            { CapacityTier.Medium, 6 },
+            { CapacityTier.High, 8 },
+        };
+    }
+}
