795ff4b67800af07b17c34d83a10e93834b4169c
PatrickManalich <patrickmanalich@gmail.com>
2022-01-02 11:05:52 -0500

    Deflect based on ShieldAbility in ShieldableAttribute script

--- GogosUnity/Assets/Scripts/Attributes/ShieldableAttribute.cs
+++ GogosUnity/Assets/Scripts/Attributes/ShieldableAttribute.cs
@@ -15,16 +15,20 @@ namespace Gogos
         protected override void OnTriggerEntered(TriggerEventArgs e)
         {
             var shieldTrigger = e.OtherCollider.GetComponent<ShieldTrigger>();
-            if (shieldTrigger != null && Player != null && shieldTrigger.Player != Player)
+            if (shieldTrigger != null)
             {
-                var shieldPosition = e.OtherCollider.transform.position;
-                var oppositeDirection = (transform.position - shieldPosition).normalized;
-                m_Rigidbody.AddForceAtPosition(oppositeDirection * DeflectForce, m_ForcePoint.transform.position, ForceMode.Impulse);
-
-                var shieldStrengthTierTracker = shieldTrigger.ShieldStrengthTierTracker;
-                if (shieldStrengthTierTracker.LastTurnModified != TurnTracker.Turn)
+                var shieldAbility = shieldTrigger.ShieldAbility;
+                if (shieldAbility.CanDeflect(Player))
                 {
-                    shieldStrengthTierTracker.ModifyTier(-1);
+                    var shieldCenterPosition = e.OtherCollider.transform.position;
+                    var awayFromCenterDirection = (transform.position - shieldCenterPosition).normalized;
+                    m_Rigidbody.AddForceAtPosition(awayFromCenterDirection * DeflectForce, m_ForcePoint.transform.position, ForceMode.Impulse);
+
+                    var shieldStrengthTierTracker = shieldTrigger.ShieldStrengthTierTracker;
+                    if (shieldStrengthTierTracker.LastTurnModified != TurnTracker.Turn)
+                    {
+                        shieldStrengthTierTracker.ModifyTier(-1);
+                    }
                 }
             }
         }
