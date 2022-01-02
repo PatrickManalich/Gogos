4f24a9efab14c827f8c0d85bb90e9732274f4c8c
PatrickManalich <patrickmanalich@gmail.com>
2022-01-02 11:00:43 -0500

    Create ShieldAbility script

---
+++ GogosUnity/Assets/Scripts/ShieldAbility.cs
@@ -0,0 +1,31 @@
+﻿using RotaryHeart.Lib.SerializableDictionaryPro;
+using System;
+using UnityEngine;
+
+namespace Gogos
+{
+    public enum ShieldResponse { Deflect }
+
+    [Serializable]
+    public class GroupsByShieldResponse : SerializableDictionary<ShieldResponse, Groups> { }
+
+    public class ShieldAbility : MonoBehaviour
+    {
+        [SerializeField]
+        private AbstractGogo m_Gogo;
+
+        [SerializeField]
+        private GroupsByShieldResponse m_GroupsByShieldResponse;
+
+        public void SetAbility(GroupsByShieldResponse groupsByShieldResponse)
+        {
+            m_GroupsByShieldResponse = groupsByShieldResponse;
+        }
+
+        public bool CanDeflect(Player player)
+        {
+            var hasDeflectGroups = m_GroupsByShieldResponse.ContainsKey(ShieldResponse.Deflect);
+            return hasDeflectGroups && m_GroupsByShieldResponse[ShieldResponse.Deflect].IsInGroup(player, m_Gogo.Player);
+        }
+    }
+}
