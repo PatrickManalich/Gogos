c563a8541250748940cd0d5f47eb877c3dd58b52
PatrickManalich <patrickmanalich@gmail.com>
2022-01-06 21:10:35 -0500

    Change how blast force is calculated

--- GogosUnity/Assets/Scripts/Attributes/BlastableAttribute.cs
+++ GogosUnity/Assets/Scripts/Attributes/BlastableAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gogos.Extensions;
+using System;
 using UnityEngine;
 
 namespace Gogos
@@ -13,7 +14,7 @@ namespace Gogos
         [SerializeField]
         private GameObject m_ForcePoint;
 
-        private const float FalloffRate = 0.4f;
+        private const float MinBlastForce = 5;
 
         protected override void OnTriggerEntered(TriggerEventArgs e)
         {
@@ -21,15 +22,16 @@ namespace Gogos
             if (blastTrigger != null && (blastTrigger.Player == null || blastTrigger.Player != Player))
             {
                 Blasted?.Invoke();
-                var blastCenterPosition = e.OtherCollider.transform.position;
-                var oppositeDirection = (transform.position - blastCenterPosition).normalized;
-                var upwardsDirection = Vector3.up * blastTrigger.BlastForceTierTracker.BlastUpwardsModifier;
-                var forceDirection = oppositeDirection + upwardsDirection;
+                var centerPosition = blastTrigger.CenterPosition;
+                var distance = Vector3.Distance(transform.position, centerPosition);
+                var radius = blastTrigger.RangeTierTracker.Range / 2;
+                var blastForce = distance.ConvertValueToDifferentRange(0, radius, blastTrigger.BlastForceTierTracker.BlastForce, MinBlastForce);
+                var awayFromCenterDirection = (transform.position - centerPosition).normalized;
+                var awayFromCenterForce = awayFromCenterDirection * blastForce;
+                var upwardsForce = Vector3.up * blastTrigger.BlastForceTierTracker.BlastUpwardsModifier;
+                var force = awayFromCenterForce + upwardsForce;
 
-                var distance = Vector3.Distance(transform.position, blastCenterPosition);
-                var distanceMultiplier = Mathf.Max(0, blastTrigger.BlastForceTierTracker.BlastForce - FalloffRate * distance);
-
-                m_Rigidbody.AddForceAtPosition(forceDirection * distanceMultiplier, m_ForcePoint.transform.position, ForceMode.Impulse);
+                m_Rigidbody.AddForceAtPosition(force, m_ForcePoint.transform.position, ForceMode.Impulse);
             }
         }
 
