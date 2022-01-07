60d8fcfca88956f85fc5aa883109bd82bb694b12
PatrickManalich <patrickmanalich@gmail.com>
2022-01-06 21:10:53 -0500

    Create DemoBlastTriggerBlaster script

---
+++ GogosUnity/Assets/Scripts/Demo/DemoBlastTriggerBlaster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class DemoBlastTriggerBlaster : MonoBehaviour
+    {
+        [SerializeField]
+        private RangeTierTracker m_RangeTierTracker;
+
+        [SerializeField]
+        private BlastForceTierTracker m_BlastForceTierTracker;
+
+        [SerializeField]
+        private BlastTrigger m_BlastTrigger;
+
+        private void Start()
+        {
+            m_BlastTrigger.Blast(m_RangeTierTracker, m_BlastForceTierTracker);
+        }
+    }
+}
