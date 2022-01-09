f1683f23081778c08f2a55277ba1ce8d690c650f
PatrickManalich <patrickmanalich@gmail.com>
2022-01-09 10:03:52 -0500

    Create TimeScaleWindow script

---
+++ GogosUnity/Assets/Scripts/Editor/Windows/TimeScaleWindow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BarronAssociatesEditor
+{
+    public class TimeScaleWindow : EditorWindow
+    {
+        private const int DefaultMinTimeScale = 0;
+        private const int DefaultMaxTimeScale = 2;
+
+        private float m_MinTimeScale = DefaultMinTimeScale;
+        private float m_MaxTimeScale = DefaultMaxTimeScale;
+
+        [MenuItem("Gogos/Window/Time Scale Window")]
+        public static void ShowWindow()
+        {
+            GetWindow<TimeScaleWindow>(false, "Time Scale");
+        }
+
+        private void OnGUI()
+        {
+            Time.timeScale = EditorGUILayout.Slider("Time Scale", Time.timeScale, m_MinTimeScale, m_MaxTimeScale);
+            m_MinTimeScale = EditorGUILayout.FloatField("Min Time Scale", m_MinTimeScale);
+            m_MaxTimeScale = EditorGUILayout.FloatField("Max Time Scale", m_MaxTimeScale);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Default"))
+            {
+                m_MinTimeScale = DefaultMinTimeScale;
+                m_MaxTimeScale = DefaultMaxTimeScale;
+                Time.timeScale = 1;
+            }
+            if (GUILayout.Button("Min Speed"))
+            {
+                Time.timeScale = m_MinTimeScale;
+            }
+            if (GUILayout.Button("Max Speed"))
+            {
+                Time.timeScale = m_MaxTimeScale;
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
