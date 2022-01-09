54790b45033326d6a27beeca92869ecfe9aff36a
PatrickManalich <patrickmanalich@gmail.com>
2022-01-09 12:06:22 -0500

    Create RootColliderIgnorer script

---
+++ GogosUnity/Assets/Scripts/Triggers/RootColliderIgnorer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class RootColliderIgnorer : MonoBehaviour
+    {
+        [SerializeField]
+        private Collider m_Collider;
+
+        private void Awake()
+        {
+            var rootCollider = transform.GetComponentInParent<Rigidbody>()?.GetComponent<Collider>();
+            if (rootCollider != null)
+            {
+                Physics.IgnoreCollision(m_Collider, rootCollider, true);
+            }
+        }
+    }
+}
