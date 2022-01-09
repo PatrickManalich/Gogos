6f267aa9ff7a5f70fe6cb1ceb05005941eca58a1
PatrickManalich <patrickmanalich@gmail.com>
2022-01-09 11:58:50 -0500

    Remove obsolete logic from TriggerListener script

--- GogosUnity/Assets/Scripts/Triggers/TriggerListener.cs
+++ GogosUnity/Assets/Scripts/Triggers/TriggerListener.cs
@@ -9,22 +9,6 @@ namespace Gogos
 
         public event EventHandler<TriggerEventArgs> Exited;
 
-        [SerializeField]
-        private bool m_IgnoreRootCollider;
-
-        private Collider m_Collider;
-
-        private void Awake()
-        {
-            m_Collider = GetComponent<Collider>();
-
-            if (m_IgnoreRootCollider)
-            {
-                var rootCollider = transform.GetComponentInParent<Rigidbody>().GetComponent<Collider>();
-                Physics.IgnoreCollision(m_Collider, rootCollider, true);
-            }
-        }
-
         private void OnTriggerEnter(Collider otherCollider)
         {
             Entered?.Invoke(this, new TriggerEventArgs(otherCollider));
