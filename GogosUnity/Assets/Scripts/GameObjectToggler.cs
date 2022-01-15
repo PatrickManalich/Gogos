8646656acb78ab0e9b772b39e0b47770b16dbdb6
PatrickManalich <patrickmanalich@gmail.com>
2022-01-15 13:09:48 -0500

    Create GameObjectToggler script

---
+++ GogosUnity/Assets/Scripts/GameObjectToggler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class GameObjectToggler : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> m_GameObjects;
+
+        [SerializeField]
+        private bool m_Value;
+
+        [SerializeField]
+        private float m_Delay;
+
+        private void OnEnable()
+        {
+            StartCoroutine(ToggleAfterDelayRoutine());
+        }
+
+        private IEnumerator ToggleAfterDelayRoutine()
+        {
+            yield return new WaitForSeconds(m_Delay);
+            m_GameObjects.ForEach(g => g.SetActive(m_Value));
+        }
+    }
+}
