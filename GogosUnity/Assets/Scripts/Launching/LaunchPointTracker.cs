959f0cb4fb174bc4fcd7d223536676e17c849cc1
PatrickManalich <patrickmanalich@gmail.com>
2022-01-17 17:42:02 -0500

    Create LaunchPointTracker script

---
+++ GogosUnity/Assets/Scripts/Launching/LaunchPointTracker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class LaunchPointTracker : MonoBehaviour
+    {
+        public List<Vector3> LaunchPoints { get; private set; } = new List<Vector3>();
+
+        [SerializeField]
+        private Checkpoint[] m_Checkpoints;
+
+        private void Start()
+        {
+            PlayerTracker.PlayerChanged += PlayerTracker_OnPlayerChanged;
+
+            var startingCheckpoints = m_Checkpoints.Where(c => c.IsStartingCheckpoint).ToList();
+            foreach (var player in PlayerTracker.Players)
+            {
+                var randomStartingCheckpoint = startingCheckpoints[Random.Range(0, startingCheckpoints.Count)];
+                randomStartingCheckpoint.SetPlayer(player);
+                startingCheckpoints.Remove(randomStartingCheckpoint);
+            }
+
+            GetLaunchPoints();
+        }
+
+        private void OnDestroy()
+        {
+            PlayerTracker.PlayerChanged -= PlayerTracker_OnPlayerChanged;
+        }
+
+        private void PlayerTracker_OnPlayerChanged()
+        {
+            GetLaunchPoints();
+        }
+
+        private void GetLaunchPoints()
+        {
+            LaunchPoints.Clear();
+
+            var checkpoints = m_Checkpoints.Where(c => c.Player == PlayerTracker.Player);
+            LaunchPoints.AddRange(checkpoints.Select(c => c.transform.position));
+
+            var playersGogos = FindObjectsOfType<AbstractGogo>().Where(g => g.Player == PlayerTracker.Player);
+            var launchedPlayerGogos = playersGogos.Where(p => GogoSituationDatabase.GetSituation(p.IdentifiableGogo) == Situation.Launched);
+            LaunchPoints.AddRange(launchedPlayerGogos.Select(l => l.transform.position));
+        }
+    }
+}
