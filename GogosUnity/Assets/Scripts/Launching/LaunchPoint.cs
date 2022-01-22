7036b8c1928da57fbad0ea5f5046a43edf9195e1
PatrickManalich <patrickmanalich@gmail.com>
2022-01-22 09:44:45 -0500

    Create LaunchPoint script

---
+++ GogosUnity/Assets/Scripts/Launching/LaunchPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class LaunchPoint
+    {
+        public int Turn { get; }
+
+        public Vector3 Position { get; }
+
+        public LaunchPoint(int turn, Vector3 position)
+        {
+            Turn = turn;
+            Position = position;
+        }
+    }
+}
