15ec65b891f50af89a09200a1dff45e13a058728
PatrickManalich <patrickmanalich@gmail.com>
2022-01-29 11:52:52 -0500

    Create AnimatedTrigger script

---
+++ GogosUnity/Assets/Scripts/Triggers/AnimatedTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class AnimatedTrigger : MonoBehaviour
+    {
+        public Player Player { get; set; }
+
+        public Vector3 CenterPosition => m_CenterPoint.transform.position;
+
+        [SerializeField]
+        private Collider m_Collider;
+
+        [SerializeField]
+        private Animator m_TriggerAnimator;
+
+        [SerializeField]
+        private GameObject m_CenterPoint;
+
+        private const string ExpandName = "Expand";
+
+        public void Expand()
+        {
+            m_Collider.enabled = true;
+            m_TriggerAnimator.SetBool(ExpandName, true);
+        }
+
+        public void Shrink()
+        {
+            m_Collider.enabled = false;
+            m_TriggerAnimator.SetBool(ExpandName, false);
+        }
+    }
+}
