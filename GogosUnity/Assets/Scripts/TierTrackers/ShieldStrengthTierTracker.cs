bebe38b6ada3a5f3b0cb2fb84392cea20f8459fd
PatrickManalich <patrickmanalich@gmail.com>
2022-02-06 12:03:26 -0500

    Remove shield alpha logic from ShieldStrengthTierTracker script

--- GogosUnity/Assets/Scripts/TierTrackers/ShieldStrengthTierTracker.cs
+++ GogosUnity/Assets/Scripts/TierTrackers/ShieldStrengthTierTracker.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Gogos
+﻿namespace Gogos
 {
     public enum ShieldStrengthTier { Broken, Weak, Medium, Strong, Unbreakable }
 
@@ -11,21 +9,11 @@ namespace Gogos
         public int LastTurnModified { get; private set; }
         public bool IsShieldBroken => Tier == ShieldStrengthTier.Broken;
         public bool IsShieldUnbreakable => Tier == ShieldStrengthTier.Unbreakable;
-        public float ShieldAlpha => ShieldAlphasByTier[Tier];
 
         public override void ModifyTier(int modifier)
         {
             base.ModifyTier(modifier);
             LastTurnModified = TurnTracker.Turn;
         }
-
-        private static readonly Dictionary<ShieldStrengthTier, float> ShieldAlphasByTier = new Dictionary<ShieldStrengthTier, float>()
-        {
-            { ShieldStrengthTier.Broken, 0 },
-            { ShieldStrengthTier.Weak, 0.1f },
-            { ShieldStrengthTier.Medium, 0.2f },
-            { ShieldStrengthTier.Strong, 0.3f },
-            { ShieldStrengthTier.Unbreakable, 0.3f },
-        };
     }
 }
