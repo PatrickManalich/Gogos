c6177e439d4b982c576b62d090e9eb33ab742dca
PatrickManalich <patrickmanalich@gmail.com>
2022-02-06 17:13:17 -0500

    Update shield and support visuals

--- GogosUnity/Assets/Scripts/Triggers/TriggerVisualColorSwapper.cs
+++ GogosUnity/Assets/Scripts/Triggers/TriggerVisualColorSwapper.cs
@@ -1,29 +1,58 @@
-﻿using Gogos.Extensions;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Gogos
 {
     public class TriggerVisualColorSwapper : MonoBehaviour
     {
         [SerializeField]
-        private MeshRenderer m_MeshRenderer;
+        private ExpandTrigger m_ExpandTrigger;
 
         [SerializeField]
-        private ExpandTrigger m_ExpandTrigger;
+        private TriggerAnimationsReference m_TriggerAnimationsReference;
+
+        [SerializeField]
+        private GameObject m_Visual;
 
         [SerializeField]
         private ScriptableColorPalette m_ScriptableColorPalette;
 
+        [SerializeField]
+        private float m_Intensity;
+
+        private Renderer[] m_Renderers = new Renderer[] { };
+
         private void Start()
         {
             m_ExpandTrigger.PlayerChanged += Refresh;
+            m_TriggerAnimationsReference.AnimationStarted += TriggerAnimationsReference_OnAnimationStarted;
+            m_TriggerAnimationsReference.AnimationFinished += TriggerAnimationsReference_OnAnimationFinished;
 
+            m_Renderers = m_Visual.GetComponentsInChildren<Renderer>(true);
+            m_Visual.SetActive(false);
             Refresh();
         }
 
         private void OnDestroy()
         {
+            m_TriggerAnimationsReference.AnimationFinished -= TriggerAnimationsReference_OnAnimationFinished;
+            m_TriggerAnimationsReference.AnimationStarted -= TriggerAnimationsReference_OnAnimationStarted;
             m_ExpandTrigger.PlayerChanged -= Refresh;
+        }
+
+        private void TriggerAnimationsReference_OnAnimationStarted(object sender, TriggerAnimationEventArgs e)
+        {
+            if (e.TriggerAnimation == TriggerAnimation.Invisible)
+            {
+                m_Visual.SetActive(false);
+            }
+        }
+
+        private void TriggerAnimationsReference_OnAnimationFinished(object sender, TriggerAnimationEventArgs e)
+        {
+            if (e.TriggerAnimation == TriggerAnimation.Expand)
+            {
+                m_Visual.SetActive(true);
+            }
         }
 
         private void Refresh()
@@ -34,9 +63,16 @@ namespace Gogos
                 return;
             }
 
-            var colorAlpha = m_MeshRenderer.material.color.a;
-            var color = m_ScriptableColorPalette.GetColorForPlayerColor(player.PlayerColor);
-            m_MeshRenderer.material.color = color.WithA(colorAlpha);
+            var playerColor = m_ScriptableColorPalette.GetColorForPlayerColor(player.PlayerColor);
+            foreach (var renderer in m_Renderers)
+            {
+                foreach (var material in renderer.materials)
+                {
+                    material.SetColor("_TintColor", playerColor * m_Intensity);
+                    material.SetColor("_Color", playerColor * m_Intensity);
+                    material.SetColor("_RimColor", playerColor * m_Intensity);
+                }
+            }
         }
     }
 }
