2a942067882600b21b3727e5a087a439a2f96476
PatrickManalich <patrickmanalich@gmail.com>
2022-02-06 22:18:47 -0500

    Create TextColorSwapper script

---
+++ GogosUnity/Assets/Scripts/UserInterface/TextColorSwapper.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class TextColorSwapper : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI m_Text;
+
+        [SerializeField]
+        private ScriptableColorPalette m_ScriptableColorPalette;
+
+        private void Start()
+        {
+            PlayerTracker.PlayerChanged += Refresh;
+
+            Refresh();
+        }
+
+        private void OnDestroy()
+        {
+            PlayerTracker.PlayerChanged -= Refresh;
+        }
+
+        private void Refresh()
+        {
+            m_Text.color = m_ScriptableColorPalette.GetColorForPlayerColor(PlayerTracker.Player.PlayerColor);
+        }
+    }
+}
