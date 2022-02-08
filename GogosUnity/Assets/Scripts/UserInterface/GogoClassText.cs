652daa619416658436d33ddf042f9a0205eba7b7
PatrickManalich <patrickmanalich@gmail.com>
2022-02-07 19:00:59 -0500

    Create GogoClassText script

---
+++ GogosUnity/Assets/Scripts/UserInterface/GogoClassText.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class GogoClassText : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI m_Text;
+
+        public void SetText(GogoClass gogoClass)
+        {
+            m_Text.text = gogoClass.ToString().Replace("Gogo", string.Empty);
+        }
+    }
+}
