3f95c5142f5e702f47dd8bc612e3ee99cc30bf31
PatrickManalich <patrickmanalich@gmail.com>
2022-02-09 23:33:45 -0500

    Create CollectionSlideout script

---
+++ GogosUnity/Assets/Scripts/CollectionSlideout.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gogos
+{
+    public class CollectionSlideout : MonoBehaviour
+    {
+        [SerializeField]
+        private Button m_OpenButton;
+
+        [SerializeField]
+        private Button m_CloseButton;
+
+        [SerializeField]
+        private GameObject m_CollectionGogoDetailsPanel;
+
+        [SerializeField]
+        private GameObject m_CollectionScrollView;
+
+        private void Start()
+        {
+            m_OpenButton.onClick.AddListener(OpenButton_OnClick);
+            m_CloseButton.onClick.AddListener(CloseButton_OnClick);
+        }
+
+        private void OnDestroy()
+        {
+            m_CloseButton.onClick.RemoveListener(CloseButton_OnClick);
+            m_OpenButton.onClick.RemoveListener(OpenButton_OnClick);
+        }
+
+        private void OpenButton_OnClick()
+        {
+            m_CloseButton.gameObject.SetActive(true);
+            m_CollectionGogoDetailsPanel.SetActive(true);
+            m_CollectionScrollView.SetActive(true);
+        }
+
+        private void CloseButton_OnClick()
+        {
+            m_CloseButton.gameObject.SetActive(false);
+            m_CollectionGogoDetailsPanel.SetActive(false);
+            m_CollectionScrollView.SetActive(false);
+        }
+    }
+}
