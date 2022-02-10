6e329993e1d1f9ab93abf7e8ffb0baf25a7256b6
PatrickManalich <patrickmanalich@gmail.com>
2022-02-10 00:26:37 -0500

    [bugfix] Face camera in Start() to prevent visual artifact

--- GogosUnity/Assets/Scripts/FaceCameraRotator.cs
+++ GogosUnity/Assets/Scripts/FaceCameraRotator.cs
@@ -4,6 +4,11 @@ namespace Gogos
 {
     public class FaceCameraRotator : MonoBehaviour
     {
+        private void Start()
+        {
+            transform.LookAt(Camera.main.transform);
+        }
+
         private void Update()
         {
             transform.LookAt(Camera.main.transform);
