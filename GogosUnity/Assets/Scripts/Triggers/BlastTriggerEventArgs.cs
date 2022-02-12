aab234f14115b51cce3d877ef631bc2de2f3d025
PatrickManalich <patrickmanalich@gmail.com>
2022-02-12 16:54:42 -0500

    Create BlastTriggerEventArgs script

---
+++ GogosUnity/Assets/Scripts/Triggers/BlastTriggerEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gogos
+{
+    public class BlastTriggerEventArgs : EventArgs
+    {
+        public BlastTrigger BlastTrigger { get; }
+
+        public BlastTriggerEventArgs(BlastTrigger blastTrigger)
+        {
+            BlastTrigger = blastTrigger;
+        }
+    }
+}
