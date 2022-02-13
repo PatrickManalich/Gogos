ff796bf1a6047a528e628a74b6245e71769c4d13
PatrickManalich <patrickmanalich@gmail.com>
2022-02-13 12:11:12 -0500

    Expose Spawn() and move method

--- GogosUnity/Assets/Scripts/Spawning/Spawner.cs
+++ GogosUnity/Assets/Scripts/Spawning/Spawner.cs
@@ -42,6 +42,15 @@ namespace Gogos
             return StartCoroutine(RandomlySpawnRoutine(spawnables));
         }
 
+        public GameObject Spawn(GameObject prefab)
+        {
+            var instance = Instantiate(prefab, transform);
+            instance.name = instance.name.Replace("(Clone)", "");
+            var randomPositionInCircle = Quaternion.Euler(90, 0, 0) * Random.insideUnitCircle * m_SpawnRadius;
+            instance.transform.SetPositionAndRotation(transform.position + randomPositionInCircle, Random.rotation);
+            return instance;
+        }
+
         private IEnumerator RandomlySpawnRoutine(Spawnable[] spawnables)
         {
             for (int i = 0; i < m_MaxSpawnAmount; i++)
@@ -54,15 +63,6 @@ namespace Gogos
                     yield return new WaitForSeconds(0.1f);
                 }
             }
-        }
-
-        private GameObject Spawn(GameObject prefab)
-        {
-            var instance = Instantiate(prefab, transform);
-            instance.name = instance.name.Replace("(Clone)", "");
-            var randomPositionInCircle = Quaternion.Euler(90, 0, 0) * Random.insideUnitCircle * m_SpawnRadius;
-            instance.transform.SetPositionAndRotation(transform.position + randomPositionInCircle, Random.rotation);
-            return instance;
         }
     }
 }
