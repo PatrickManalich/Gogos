844fce74bbbbdb8e5048bbb6cde1c9c13027c868
PatrickManalich <patrickmanalich@gmail.com>
2022-02-13 10:51:10 -0500

    Create ShieldVisualRefresher script

---
+++ GogosUnity/Assets/Scripts/Refreshers/ShieldVisualRefresher.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class ShieldVisualRefresher : MonoBehaviour
+    {
+        [SerializeField]
+        private ShieldStrengthTierTracker m_ShieldStrengthTierTracker;
+
+        [SerializeField]
+        private GameObject m_ShieldVisual;
+
+        private void OnEnable()
+        {
+            m_ShieldStrengthTierTracker.TierChanged += Refresh;
+
+            Refresh();
+        }
+
+        private void OnDisable()
+        {
+            m_ShieldStrengthTierTracker.TierChanged -= Refresh;
+        }
+
+        public void Refresh()
+        {
+            m_ShieldVisual.gameObject.SetActive(!m_ShieldStrengthTierTracker.IsShieldBroken);
+        }
+    }
+}
