b82ac8e48903e09e3f4768746702dbaf80b7faf6
PatrickManalich <patrickmanalich@gmail.com>
2022-02-13 11:59:57 -0500

    Create GoldenMolly script

---
+++ GogosUnity/Assets/Scripts/Gogos/GoldenGogos/GoldenMolly.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class GoldenMolly : AbstractGogo
+    {
+        [SerializeField]
+        private SupportAbility m_SupportAbility;
+
+        [SerializeField]
+        private GroundSnapper m_TriggerRangeGroundSnapper;
+
+        [SerializeField]
+        private SupportTrigger m_SupportTrigger;
+
+        public override void SetPlayer(Player player)
+        {
+            base.SetPlayer(player);
+            m_SupportAbility.SetPlayer(Player);
+            m_SupportTrigger.SetPlayer(Player);
+        }
+
+        public override void SetTiers(IdentifiableGogo identifiableGogo)
+        {
+            base.SetTiers(identifiableGogo);
+            var supportScriptableGogo = (SupportScriptableGogo)IdentifiableGogo.ScriptableGogo;
+            m_SupportAbility.SetAbility(supportScriptableGogo.Groups, supportScriptableGogo.SupportAbilityTierVariant, supportScriptableGogo.SupportAbilityTierModifier);
+        }
+
+        protected override void OnStartedMoving()
+        {
+            ReparentTriggerRange();
+            m_SupportTrigger.RemoveSupport();
+        }
+
+        protected override void OnStoppedMoving()
+        {
+            m_TriggerRangeGroundSnapper.SnapToGround();
+            UnparentTriggerRange();
+            m_SupportTrigger.ProvideSupport(m_SupportAbility);
+        }
+    }
+}
