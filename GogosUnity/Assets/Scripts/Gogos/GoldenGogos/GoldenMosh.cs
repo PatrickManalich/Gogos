bbd95b33686f9209bfa44960c259e2ca0035c15a
PatrickManalich <patrickmanalich@gmail.com>
2022-02-13 11:17:05 -0500

    Create GoldenMosh scripts

---
+++ GogosUnity/Assets/Scripts/Gogos/GoldenGogos/GoldenMosh.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class GoldenMosh : AbstractGogo
+    {
+        [SerializeField]
+        private ShieldAbility m_ShieldAbility;
+
+        [SerializeField]
+        private GroundSnapper m_TriggerRangeGroundSnapper;
+
+        [SerializeField]
+        private ShieldTrigger m_ShieldTrigger;
+
+        private RangeTierTracker m_RangeTierTracker;
+        private ShieldStrengthTierTracker m_ShieldStrengthTierTracker;
+
+        protected override void Start()
+        {
+            base.Start();
+            m_RangeTierTracker = (RangeTierTracker)TierTrackerReference.GetTierTrackerForVariant(TierVariant.Range);
+            m_ShieldStrengthTierTracker = (ShieldStrengthTierTracker)TierTrackerReference.GetTierTrackerForVariant(TierVariant.ShieldStrength);
+        }
+
+        protected override void OnStartedMoving()
+        {
+            ReparentTriggerRange();
+            m_ShieldTrigger.DisableShield();
+        }
+
+        protected override void OnStoppedMoving()
+        {
+            if (m_ShieldStrengthTierTracker.IsShieldBroken)
+            {
+                return;
+            }
+
+            m_TriggerRangeGroundSnapper.SnapToGround();
+            UnparentTriggerRange();
+            m_ShieldTrigger.EnableShield(m_RangeTierTracker, m_ShieldStrengthTierTracker, m_ShieldAbility);
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/Refreshers/GoldenMoshShieldRefresher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class GoldenMoshShieldRefresher : MonoBehaviour
+    {
+        [SerializeField]
+        private ShieldStrengthTierTracker m_ShieldStrengthTierTracker;
+
+        [SerializeField]
+        private RangeTierTracker m_RangeTierTracker;
+
+        private static readonly Dictionary<ShieldStrengthTier, RangeTier> RangeTiersByShieldStrengthTier = new Dictionary<ShieldStrengthTier, RangeTier>()
+        {
+            { ShieldStrengthTier.Broken, RangeTier.Small },
+            { ShieldStrengthTier.Weak, RangeTier.Small },
+            { ShieldStrengthTier.Medium, RangeTier.Medium },
+            { ShieldStrengthTier.Strong, RangeTier.Large },
+            { ShieldStrengthTier.Unbreakable, RangeTier.Large },
+        };
+
+        private void OnEnable()
+        {
+            m_ShieldStrengthTierTracker.TierChanged += Refresh;
+
+            Refresh();
+        }
+
+        private void OnDisable()
+        {
+            m_ShieldStrengthTierTracker.TierChanged -= Refresh;
+        }
+
+        public void Refresh()
+        {
+            var rangeTier = RangeTiersByShieldStrengthTier[m_ShieldStrengthTierTracker.Tier];
+            m_RangeTierTracker.SetTier((int)rangeTier);
+        }
+    }
+}
