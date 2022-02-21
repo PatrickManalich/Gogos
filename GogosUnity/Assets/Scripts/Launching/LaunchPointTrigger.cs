ca3c0001864ad19b1d06e2c8775cdb79dd92e830
PatrickManalich <patrickmanalich@gmail.com>
2022-02-20 20:41:03 -0500

    Remove shield from LaunchPointTrigger prefab

--- GogosUnity/Assets/Scripts/Launching/LaunchPointTrigger.cs
+++ GogosUnity/Assets/Scripts/Launching/LaunchPointTrigger.cs
@@ -18,15 +18,6 @@ namespace Gogos
         private GameObject m_LaunchPointMarker;
 
         [SerializeField]
-        private ShieldAbility m_ShieldAbility;
-
-        [SerializeField]
-        private TierTrackerReference m_TierTrackerReference;
-
-        [SerializeField]
-        private ShieldTrigger m_ShieldTrigger;
-
-        [SerializeField]
         private bool m_IsStartingTrigger;
 
         private void Start()
@@ -44,12 +35,6 @@ namespace Gogos
             Player = player;
             m_TriggerListener.DisableTrigger();
             m_LaunchPointMarker.SetActive(false);
-
-            m_ShieldAbility.SetPlayer(Player);
-            m_ShieldTrigger.SetPlayer(Player);
-            var rangeTierTracker = (RangeTierTracker)m_TierTrackerReference.GetTierTrackerForVariant(TierVariant.Range);
-            var shieldStrengthTierTracker = (ShieldStrengthTierTracker)m_TierTrackerReference.GetTierTrackerForVariant(TierVariant.ShieldStrength);
-            m_ShieldTrigger.EnableShield(rangeTierTracker, shieldStrengthTierTracker, m_ShieldAbility);
         }
 
         private void TriggerListener_OnEntered(object sender, TriggerEventArgs e)
