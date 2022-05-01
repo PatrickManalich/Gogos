c2b97bd00eee661e9cb26aa479400855c8d59f5b
PatrickManalich <patrickmanalich@gmail.com>
2022-05-01 19:32:21 -0400

    Create GoldenScriptableGogo script

---
+++ GogosUnity/Assets/Scripts/ScriptableObjects/GoldenScriptableGogo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    [CreateAssetMenu(fileName = "GoldenGogo", menuName = "Gogos/Scriptable Gogo/Golden")]
+    public class GoldenScriptableGogo : AbstractScriptableGogo
+    {
+        public override GogoClass GogoClass => GogoClass.Golden;
+    }
+}
