649a6158ff4871f4f89a78c3380a8c8b2425d38c
PatrickManalich <patrickmanalich@gmail.com>
2022-05-06 17:11:44 -0400

    Create ObjectiveTracker script

---
+++ GogosUnity/Assets/Scripts/Singletons/ObjectiveTracker.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Gogos
+{
+    public enum Objective { Collect, Defeat }
+
+    public class ObjectiveTracker : AbstractSingleton<TurnTracker>
+    {
+        public static event Action ObjectiveChanged;
+
+        public static Objective Objective { get; private set; }
+
+        [SerializeField]
+        private SpawnerRandomizer m_SpawnerRandomizer;
+
+        private int m_SwitchObjectivesTurn;
+        private CollectableAttribute m_GoldenGogoCollectableAttribute;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Objective = Objective.Collect;
+            m_SwitchObjectivesTurn = PlayerTracker.PlayerCount * 3 + 1;
+        }
+
+        private void Start()
+        {
+            TurnTracker.TurnChanged += TurnTracker_OnTurnChanged;
+            m_SpawnerRandomizer.Spawned += SpawnerRandomizer_OnSpawned;
+        }
+
+        private void OnDestroy()
+        {
+            ClearAttributeAndUnsubscribe();
+            m_SpawnerRandomizer.Spawned -= SpawnerRandomizer_OnSpawned;
+            TurnTracker.TurnChanged -= TurnTracker_OnTurnChanged;
+        }
+
+        private void TurnTracker_OnTurnChanged()
+        {
+            if (Objective == Objective.Collect && TurnTracker.Turn == m_SwitchObjectivesTurn)
+            {
+                Objective = Objective.Defeat;
+                ObjectiveChanged?.Invoke();
+            }
+        }
+
+        private void SpawnerRandomizer_OnSpawned()
+        {
+            if (Objective == Objective.Defeat && m_GoldenGogoCollectableAttribute == null)
+            {
+                FindAttributeAndSubscribe();
+            }
+        }
+
+        private void GoldenGogoCollectableAttribute_OnCollected()
+        {
+            ClearAttributeAndUnsubscribe();
+            m_SwitchObjectivesTurn = TurnTracker.Turn + PlayerTracker.PlayerCount * 3 + 1;
+            Objective = Objective.Collect;
+            ObjectiveChanged?.Invoke();
+        }
+
+        private void FindAttributeAndSubscribe()
+        {
+            var goldenGogo = FindObjectsOfType<AbstractGogo>().First(g => g.IdentifiableGogo.ScriptableGogo.GogoClass == GogoClass.Golden);
+            m_GoldenGogoCollectableAttribute = goldenGogo.GetComponentInChildren<CollectableAttribute>();
+            m_GoldenGogoCollectableAttribute.Collected += GoldenGogoCollectableAttribute_OnCollected;
+        }
+
+        private void ClearAttributeAndUnsubscribe()
+        {
+            if (m_GoldenGogoCollectableAttribute != null)
+            {
+                m_GoldenGogoCollectableAttribute.Collected -= GoldenGogoCollectableAttribute_OnCollected;
+                m_GoldenGogoCollectableAttribute = null;
+            }
+        }
+    }
+}
