ee4ded5a5fee08aed4de5f5fec86246db06efb0c
PatrickManalich <patrickmanalich@gmail.com>
2022-05-15 19:30:31 -0400

    Add gems to Collection script

--- GogosUnity/Assets/Scripts/Collection.cs
+++ GogosUnity/Assets/Scripts/Collection.cs
@@ -8,14 +8,36 @@ namespace Gogos
     {
         public event EventHandler<IdentifiableGogoEventArgs> GogoAdded;
 
+        public event Action GemsAdded;
+
         public IReadOnlyList<IdentifiableGogo> IdentifiableGogos => m_IdentifiableGogosById.Values.ToList();
 
         private Dictionary<string, IdentifiableGogo> m_IdentifiableGogosById = new Dictionary<string, IdentifiableGogo>();
+
+        private Dictionary<GogoClass, int> m_GemsByGogoClass = new Dictionary<GogoClass, int>();
+
+        public Collection()
+        {
+            m_GemsByGogoClass.Add(GogoClass.Blast, 0);
+            m_GemsByGogoClass.Add(GogoClass.Shield, 0);
+            m_GemsByGogoClass.Add(GogoClass.Support, 0);
+        }
 
         public void Add(IdentifiableGogo identifiableGogo)
         {
             m_IdentifiableGogosById.Add(identifiableGogo.Id, identifiableGogo);
             GogoAdded?.Invoke(this, new IdentifiableGogoEventArgs(identifiableGogo));
+        }
+
+        public void Add(GogoClass gogoClass, int gems)
+        {
+            m_GemsByGogoClass[gogoClass] += gems;
+            GemsAdded?.Invoke();
+        }
+
+        public int GetGemsForGogoClass(GogoClass gogoClass)
+        {
+            return m_GemsByGogoClass[gogoClass];
         }
     }
 }
