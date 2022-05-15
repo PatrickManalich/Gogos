de68b123b64fdb0f60dee44861761bb8aa606a6e
PatrickManalich <patrickmanalich@gmail.com>
2022-05-15 19:30:12 -0400

    Create GemValueTierTracker script

---
+++ GogosUnity/Assets/Scripts/TierTrackers/GemValueTierTracker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gogos
+{
+    public enum GemValueTier { Low, Medium, High }
+
+    public class GemValueTierTracker : AbstractTierTracker<GemValueTier>
+    {
+        public override TierVariant TierVariant => TierVariant.GemValue;
+
+        public int GemValue => GemValuesByTier[Tier];
+
+        public GogoClass GogoClass => m_GogoClass;
+
+        [SerializeField]
+        private GogoClass m_GogoClass;
+
+        private static readonly Dictionary<GemValueTier, int> GemValuesByTier = new Dictionary<GemValueTier, int>()
+        {
+            { GemValueTier.Low, 5 },
+            { GemValueTier.Medium, 10 },
+            { GemValueTier.High, 20 },
+        };
+    }
+}
