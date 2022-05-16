a85f14455211ab433b665e2f9f7ab441a529311b
PatrickManalich <patrickmanalich@gmail.com>
2022-05-15 22:09:45 -0400

    Create GemCollectedEffect script

---
+++ GogosUnity/Assets/Scripts/Effects/GemCollectedEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class GemCollectedEffect : AbstractCollectedEffect
+    {
+        [SerializeField]
+        private GemValueTierTracker m_GemValueTierTracker;
+
+        protected override void OnCollected()
+        {
+            if (PhaseTracker.Phase != Phase.Settling)
+            {
+                return;
+            }
+
+            PlayerTracker.Player.Collection.Add(m_GemValueTierTracker.GogoClass, m_GemValueTierTracker.GemValue);
+        }
+    }
+}
