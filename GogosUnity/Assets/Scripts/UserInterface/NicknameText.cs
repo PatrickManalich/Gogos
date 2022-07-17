1a4021a97a8a6b18c7035b8d6def7f0f51cb60b7
PatrickManalich <patrickmanalich@gmail.com>
2022-07-17 17:21:48 -0400

    [refactor] Use ScriptableColorPalette in NicknameText script

--- GogosUnity/Assets/Scripts/UserInterface/NicknameText.cs
+++ GogosUnity/Assets/Scripts/UserInterface/NicknameText.cs
@@ -1,24 +1,20 @@
-﻿using RotaryHeart.Lib.SerializableDictionaryPro;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 namespace Gogos
 {
     public class NicknameText : MonoBehaviour
     {
-        [System.Serializable]
-        private class ColorsByTier : SerializableDictionary<RarityTier, Color> { }
-
         [SerializeField]
         private TextMeshProUGUI m_Text;
 
         [SerializeField]
-        private ColorsByTier m_ColorsByTier;
+        private ScriptableColorPalette m_ScriptableColorPalette;
 
         public void SetText(string text, RarityTier rarityTier)
         {
             m_Text.text = text;
-            m_Text.color = m_ColorsByTier[rarityTier];
+            m_Text.color = m_ScriptableColorPalette.GetColorForRarityTier(rarityTier);
         }
     }
 }
