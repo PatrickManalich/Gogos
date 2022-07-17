05150df674f52d2118b0291bdbeb265db778e34e
PatrickManalich <patrickmanalich@gmail.com>
2022-07-17 19:16:54 -0400

    Create sparkle visual scripts

---
+++ GogosUnity/Assets/Scripts/Refreshers/SparkleVisualRefresher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class SparkleVisualRefresher : MonoBehaviour
+    {
+        [SerializeField]
+        private RarityTierTracker m_RarityTierTracker;
+
+        [SerializeField]
+        private ScriptableColorPalette m_ScriptableColorPalette;
+
+        [SerializeField]
+        private SparkleVisual m_SparkleVisual;
+
+        private void OnEnable()
+        {
+            m_RarityTierTracker.TierChanged += Refresh;
+
+            Refresh();
+        }
+
+        private void OnDisable()
+        {
+            m_RarityTierTracker.TierChanged -= Refresh;
+        }
+
+        private void Refresh()
+        {
+            var color = m_ScriptableColorPalette.GetColorForRarityTier(m_RarityTierTracker.Tier);
+            m_SparkleVisual.SetColor(color);
+        }
+    }
+}
---
+++ GogosUnity/Assets/Scripts/SparkleVisual.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class SparkleVisual : MonoBehaviour
+    {
+        [SerializeField]
+        private AbstractGogo m_Gogo;
+
+        [SerializeField]
+        private GameObject m_Visual;
+
+        private const float DestroyDelay = 2;
+
+        private ParticleSystem[] m_ParticleSystems;
+
+        private void Update()
+        {
+            m_Visual.SetActive(m_Gogo.Player == null);
+        }
+
+        public void SetColor(Color color)
+        {
+            foreach (var particleSystem in GetParticleSystems())
+            {
+                var main = particleSystem.main;
+                main.startColor = color;
+            }
+        }
+
+        public void StopAndDestroy()
+        {
+            foreach (var particleSystem in GetParticleSystems())
+            {
+                particleSystem.Stop();
+            }
+            transform.parent = null;
+            Destroy(gameObject, DestroyDelay);
+        }
+
+        private ParticleSystem[] GetParticleSystems()
+        {
+            if (m_ParticleSystems == null)
+            {
+                m_ParticleSystems = m_Visual.GetComponentsInChildren<ParticleSystem>(true);
+            }
+            return m_ParticleSystems;
+        }
+    }
+}
