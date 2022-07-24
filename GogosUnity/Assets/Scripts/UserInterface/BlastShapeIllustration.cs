908c5352a3dc33808883dceac39727d62412a704
PatrickManalich <patrickmanalich@gmail.com>
2022-07-24 11:10:39 -0400

    [bugfix] Continue playing illustration after toggled

--- GogosUnity/Assets/Scripts/UserInterface/BlastShapeIllustration.cs
+++ GogosUnity/Assets/Scripts/UserInterface/BlastShapeIllustration.cs
@@ -16,25 +16,46 @@ namespace Gogos
 
         private Coroutine m_IllustrationCoroutine;
 
-        public void SetIllustration(Sprite[] sprites)
+        private Sprite[] m_Sprites;
+
+        private void OnEnable()
+        {
+            if (m_Sprites != null)
+            {
+                m_IllustrationCoroutine = StartCoroutine(IllustrationRoutine());
+            }
+        }
+
+        private void OnDisable()
         {
             if (m_IllustrationCoroutine != null)
             {
                 StopCoroutine(m_IllustrationCoroutine);
                 m_IllustrationCoroutine = null;
             }
-
-            m_IllustrationCoroutine = StartCoroutine(IllustrationRoutine(sprites));
         }
 
-        private IEnumerator IllustrationRoutine(Sprite[] sprites)
+        public void SetIllustration(Sprite[] sprites)
+        {
+            m_Sprites = sprites;
+
+            if (m_IllustrationCoroutine != null)
+            {
+                StopCoroutine(m_IllustrationCoroutine);
+                m_IllustrationCoroutine = null;
+            }
+
+            m_IllustrationCoroutine = StartCoroutine(IllustrationRoutine());
+        }
+
+        private IEnumerator IllustrationRoutine()
         {
             while (true)
             {
                 m_Image.sprite = m_EmptySprite;
                 yield return new WaitForSeconds(1);
 
-                foreach (var sprite in sprites)
+                foreach (var sprite in m_Sprites)
                 {
                     m_Image.sprite = sprite;
                     yield return new WaitForSeconds(SecondsBetweenSprites);
