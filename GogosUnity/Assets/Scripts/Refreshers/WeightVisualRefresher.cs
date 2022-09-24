69dce2cc6292c5a7db9e779d990544f166aa5db3
PatrickManalich <patrickmanalich@gmail.com>
2022-09-23 23:12:44 -0400

    Create WeightVisualRefresher script

---
+++ GogosUnity/Assets/Scripts/Refreshers/WeightVisualRefresher.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gogos
+{
+    public class WeightVisualRefresher : MonoBehaviour
+    {
+        [SerializeField]
+        private WeightTierTracker m_WeightTierTracker;
+
+        [SerializeField]
+        private BoxCollider m_BoxCollider;
+
+        [SerializeField]
+        private GameObject m_Visuals;
+
+        private static readonly Dictionary<WeightTier, float> ScalarsByTier = new Dictionary<WeightTier, float>()
+        {
+            { WeightTier.Lightweight, 1 },
+            { WeightTier.Middleweight, 1.25f },
+            { WeightTier.Heavyweight, 1.5f },
+        };
+
+        private Vector3 m_OriginalBoxColliderSize;
+
+        private void Awake()
+        {
+            m_OriginalBoxColliderSize = m_BoxCollider.size;
+        }
+
+        private void OnEnable()
+        {
+            m_WeightTierTracker.TierChanged += Refresh;
+
+            Refresh();
+        }
+
+        private void OnDisable()
+        {
+            m_WeightTierTracker.TierChanged -= Refresh;
+        }
+
+        private void Refresh()
+        {
+            var scalar = ScalarsByTier[m_WeightTierTracker.Tier];
+            m_BoxCollider.size = m_OriginalBoxColliderSize * scalar;
+            m_Visuals.transform.localScale = Vector3.one * scalar;
+        }
+    }
+}
