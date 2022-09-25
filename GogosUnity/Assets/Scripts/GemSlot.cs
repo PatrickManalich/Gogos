15117a86bd293f3a33312ea76bbcbf1d09180a6a
PatrickManalich <patrickmanalich@gmail.com>
2022-09-25 11:25:15 -0400

    Create GemSlot script

---
+++ GogosUnity/Assets/Scripts/GemSlot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gogos
+{
+    public class GemSlot : MonoBehaviour
+    {
+        public Transform ReleaseParent { get; set; }
+
+        private Rigidbody m_Rigidbody;
+
+        private void Awake()
+        {
+            m_Rigidbody = GetComponentInChildren<Rigidbody>();
+            m_Rigidbody.isKinematic = true;
+        }
+
+        public void Release()
+        {
+            m_Rigidbody.isKinematic = false;
+            transform.parent = ReleaseParent;
+        }
+    }
+}
